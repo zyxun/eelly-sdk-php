7e3df1db1cb5b955b7d0ef84cffc90771d62b137
liangxinyi <liangxinyi@eelly.net>
2017-07-20 13:02:49 +0800

    去掉SDK中oauth模块的前缀

---
+++ src/SDK/Oauth/Api/Role.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * PHP version 7.1
+ *
+ * @copyright Copyright (c) 2012-2017 EELLY Inc. (https://www.eelly.com)
+ * @link      https://api.eelly.com
+ * @license   衣联网版权所有
+ */
+
+namespace Eelly\SDK\Oauth\Api;
+
+use Eelly\SDK\EellyClient;
+use Eelly\SDK\Oauth\Service\RoleInterface;
+
+/**
+ *
+ * @author eellytools<localhost.shell@gmail.com>
+ */
+class Role implements RoleInterface
+{
+
+    /**
+     * 获得角色列表.
+     * @return array
+     */
+    public function getRoleList(): array
+    {
+        return EellyClient::request('oauth/roleserver', 'getRoleList');
+    }
+
+    /**
+     * @param int $id 角色id
+     * @param array $data 更新数组
+     * @return int 数据库更新返回值
+     */
+    public function updateRole(int $id,array $data): int
+    {
+        return EellyClient::request('oauth/roleserver', 'updateRole', $id, $data);
+    }
+
+    /**
+     * 新增角色
+     * @param array $data 数组
+     * @return int 数据库新增返回id
+     */
+    public function addRole(array $data): int
+    {
+        return EellyClient::request('oauth/roleserver', 'addRole', $data);
+    }
+
+
+    /**
+     * 新增角色与客户端对应关系
+     * @param array $data
+     * @return int
+     */
+    public function addRoleClient($data): int
+    {
+        // TODO: Implement addRoleClient() method.
+        return EellyClient::request('oauth/roleserver', 'addRoleClient', $data);
+    }
+
+    /**
+     * 新增角色权限操作
+     * @param array $data
+     * @return int
+     */
+    public function addRolePermission(array $data): int
+    {
+        // TODO: Implement addRolePermission() method.
+        return EellyClient::request('oauth/roleserver', 'addRolePermission', $data);
+
+    }
+
+    /**
+     * @param int $id 角色id
+     * @param array $data 更新角色权限数组
+     * @return int 数据库更新返回值
+     */
+    public function updateRolePermission(int $id, array $data): bool
+    {
+        // TODO: Implement updateRolePermission() method.
+        return EellyClient::request('oauth/roleserver', 'updateRolePermission',$id, $data);
+
+    }
+}
---
+++ src/SDK/Oauth/Service/ClientInterface.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\Oauth\Service;
+use Eelly\SDK\Oauth\Service\Index\DTO\ClientDTO;
+use Eelly\SDK\Oauth\Service\Index\DTO\ResultDTO;
+
+/**
+ * @author liangxinyi<liangxinyi@eelly.net>
+ */
+interface ClientInterface
+{
+    /**
+     * 添加客户端.
+     *
+     * @return int
+     * @author liangxinyi<liangxinyi@eelly.net>
+     */
+    public function addClient(array $data):int ;
+
+    /**
+     * 分页获得客户端列表.
+     *
+     * @param int $limit   每页页数
+     * @param int $currentPage  当前页
+     * @return array
+     * @author liangxinyi<liangxinyi@eelly.net>
+     */
+    public function listClientPage(int $limit=10,int $currentPage=1):array;
+
+
+    /**
+     * 获得客户端列表.
+     *
+     * @return array
+     * @author liangxinyi<liangxinyi@eelly.net>
+     */
+    public function listClient():array;
+
+    /**
+     * 编辑客户端.
+     *
+     * @param int $id      客户端id
+     * @param array $data  更新信息
+     * @return bool
+     * @author liangxinyi<liangxinyi@eelly.net>
+     */
+    public function updateClient(int $id,array $data):bool ;
+
+
+    /**
+     * 根据id获取客户端信息.
+     *
+     * @param int $id  客户端id
+     * @return ClientDTO
+     * @returnExample({"client_id":"1","client_key":"myawesomeapp","client_secret":"$2y$10$ZhlBMQNOUNSWt95LNMIfqePECBg85zwqjq7xS56uzQCllnQ4T9sgG","is_encrypt":"\u0001","user_id":"0","org_name":"eelly","app_name":"myapp","redirect_uri":"","auth_type":"4","created_time":"0","update_time":"2017-06-13 14:29:53"})
+     * @author liangxinyi<liangxinyi@eelly.net>
+     */
+    public function getClient(int $id):ClientDTO ;
+
+
+}
