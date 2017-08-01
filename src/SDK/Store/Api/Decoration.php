40f1f3813e3039553c4db5e8f0afc855081c5a74
fenghaikun <fenghaikun@eelly.net>
2017-08-01 17:21:56 +0800

    初始化文件-待开发

---
+++ src/SDK/Store/Api/Decoration.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * PHP version 7.1
+ *
+ * @copyright Copyright (c) 2012-2017 EELLY Inc. (https://www.eelly.com)
+ * @link      https://api.eelly.com
+ * @license   衣联网版权所有
+ */
+
+namespace Eelly\SDK\Store\Api;
+
+use Eelly\SDK\EellyClient;
+use Eelly\SDK\Store\Service\DecorationInterface;
+
+/**
+ *
+ * @author eellytools<localhost.shell@gmail.com>
+ */
+class Decoration implements DecorationInterface
+{
+
+    /**
+     * @return self
+     */
+    public static function getInstance() :self
+    {
+        static $instance;
+        if (null === $instance) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+}
---
+++ src/SDK/User/Api/Bind.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * PHP version 7.1
+ *
+ * @copyright Copyright (c) 2012-2017 EELLY Inc. (https://www.eelly.com)
+ * @link      https://api.eelly.com
+ * @license   衣联网版权所有
+ */
+
+namespace Eelly\SDK\User\Api;
+
+use Eelly\SDK\EellyClient;
+use Eelly\SDK\User\Service\BindInterface;
+
+/**
+ *
+ * @author eellytools<localhost.shell@gmail.com>
+ */
+class Bind implements BindInterface
+{
+
+    /**
+     * @return self
+     */
+    public static function getInstance() :self
+    {
+        static $instance;
+        if (null === $instance) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+}
