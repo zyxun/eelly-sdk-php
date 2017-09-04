17bd44215b673932422fd72cdd5a9cb0d6f6287b
laraveljun <xiaojunming@eelly.net>
2017-09-04 15:01:55 +0800

    商品操作log

--- src/SDK/Log/Service/GoodsHandleInterface.php
+++ src/SDK/Log/Service/GoodsHandleInterface.php
@@ -38,18 +38,20 @@ interface GoodsHandleInterface
     /**
      * 商品操作日志.
      *
-     * @param array $data 日志数据
      * @param int $data ['type'] 商品操作类型
      * @param int $data ['goodsId'] 商品ID
      * @param string $data ['name'] 商品名
      * @param int $data ['adminId'] 管理员ID
      * @param string $data ['adminName'] 管理员名
      * @param string $data ['remark']  备注 json 格式保存为了以后查询方便
+     * @param array $data 日志数据
      *
      * @throws \Eelly\SDK\Log\Exception\GoodsHandleException
+     *
      * @return bool true|false
-     * @requestExample({"type":{"type":"1", "goodsId":"商品ID不能为空","adminId":"1","remark":{'handle_page':'index.html'} }})
+     * @requestExample({"data":{"type":"1", "goodsId":"商品ID不能为空","adminId":"1","remark":{'handle_page':'index.html'} }})
      * @returnExample(true)
+     *
      * @author 肖俊明<xiaojunming@eelly.net>
      *
      * @since 2017年08月26日
@@ -69,10 +71,24 @@ interface GoodsHandleInterface
     //public function deleteGoodsHandle(int $GoodsHandleId): bool;
 
     /**
+     * 分页获取操作信息.
      *
-     * @author eellytools<localhost.shell@gmail.com>
+     * @param int $type 类型
+     * @param int $goodsId 商品id
+     * @param string $name 商品名
+     * @param int $adminId 管理员ID
+     * @param string $adminName 管理员名称
+     * @param int $currentPage  第几页
+     * @param int $limit  条数
+     * @throws \Eelly\SDK\Log\Exception\GoodsHandleException
+     *
+     * @return array
+     * @requestExample({"type":"1", "goodsId":"1","name":"商品名","adminId":1,"adminName":"admin"} )
+     * @returnExample({"type":"1", "goodsId":"商品ID不能为空","adminId":"1","remark":{'handle_page':'index.html'} })
+     * @author 肖俊明<xiaojunming@eelly.net>
+     * @since 2017年09月04日
      */
-    //public function listGoodsHandlePage(array $condition = [], int $limit = 10, int $currentPage = 1): array;
+    public function listGoodsHandlePage(int $type = 0, int $goodsId = 0, string $name = '', int $adminId = 0, string $adminName = '', int $currentPage = 1, int $limit = 10): array ;
 
 
 }
