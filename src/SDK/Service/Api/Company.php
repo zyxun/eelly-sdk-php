13e3f9aac6d3e66e9d19ef6701bd5f9ce1b3ce6c
wujunhua <330496248@qq.com>
2017-09-05 15:51:40 +0800

    店铺企业认证相关

--- src/SDK/Service/Api/Company.php
+++ src/SDK/Service/Api/Company.php
@@ -13,13 +13,53 @@ declare(strict_types=1);
 
 namespace Eelly\SDK\Service\Api;
 
+use Eelly\SDK\EellyClient;
 use Eelly\SDK\Service\Service\CompanyInterface;
+use Eelly\DTO\UidDTO;
+use Eelly\SDK\Service\DTO\CompanyDTO;
 
 /**
  * @author eellytools<localhost.shell@gmail.com>
  */
 class Company implements CompanyInterface
 {
+
+    /**
+     *
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function getCompany(int $storeId): CompanyDTO
+    {
+        return EellyClient::request('service/Company', 'getCompany', $storeId);
+    }
+
+    /**
+     *
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function addCompany(array $data, UidDTO $user = null): bool
+    {
+        return EellyClient::request('service/Company', 'addCompany', $data, $user);
+    }
+
+    /**
+     *
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function updateCompany(array $data, UidDTO $user = null): bool
+    {
+        return EellyClient::request('service/Company', 'updateCompany', $data, $user);
+    }
+
+    /**
+     *
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function checkCompany(int $storeId, UidDTO $user = null): bool
+    {
+        return EellyClient::request('service/Company', 'checkCompany', $storeId);
+    }
+
     /**
      * @return self
      */
--- src/SDK/Service/Service/CompanyInterface.php
+++ src/SDK/Service/Service/CompanyInterface.php
@@ -14,37 +14,48 @@ declare(strict_types=1);
 namespace Eelly\SDK\Service\Service;
 
 use Eelly\DTO\UidDTO;
+use Eelly\SDK\Service\DTO\CompanyDTO;
 
 /**
  * @author eellytools<localhost.shell@gmail.com>
  */
 interface CompanyInterface
 {
+
+    /**
+     * 获取一条店铺企业认证记录.
+     *
+     * @param int $storeId 店铺ID
+     *
+     * @return CompanyDTO
+     * @requestExample({"storeId":1})
+     * @returnExample({"storeId":1,"name":"\u5e97\u94fa1","license":"440981198806232871","mobile":"13427587735","company":"sixdec","number":"12345678899999","businessLicense":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","companyPhoto":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","status":1,"created_time":1458093605})
+     * @throws \Eelly\SDK\Service\Exception\CompanyException
+     * @author wujunhua<wujunhua@eelly.net>
+     * @since 2017-09-04
+     */
+    public function getCompany(int $storeId): CompanyDTO;
+
     /**
      * 新增店铺企业认证数据.
      *
-     * @param array  $data                     认证数据
-     * @param int    $data['store_id']
-     * @param int    $data['name']
-     * @param int    $data['license']
-     * @param int    $data['mobile']
-     * @param int    $data['company']
-     * @param int    $data['number']
-     * @param int    $data['business_license']
-     * @param int    $data['company_photo']
-     * @param UidDTO $user                     登录用户对象
-     *
-     * @throws Eelly\SDK\Service\Exception\CompanyException 701001 参数错误
-     * @throws Eelly\SDK\Service\Exception\CompanyException 700001 用户未登录
-     * @throws Eelly\SDK\Service\Exception\CompanyException 702001 数据不存在
+     * @param array  $data                    认证数据
+     * @param int    $data['storeId']         店铺id
+     * @param string $data['name']            真实姓名
+     * @param string $data['license']         身份证号码
+     * @param string $data['mobile']          手机号
+     * @param string $data['company']         企业名称
+     * @param string $data['number']          工商注册号
+     * @param string $data['businessLicense'] 营业执照图片地址，最多5张
+     * @param string $data['companyPhoto']    企业实拍图片地址，最多5张
+     * @param UidDTO $user                    登录用户对象
      *
      * @return bool
-     * @requestExample({"data":{"store_id":1,"name":"\u5e97\u94fa1","license":"440981198806232871","mobile":"13427587735","company":"sixdec","number":"12345678899999","business_license":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","company_photo":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]"}})
+     * @requestExample({"data":{"storeId":1,"name":"\u5e97\u94fa1","license":"440981198806232871","mobile":"13427587735","company":"sixdec","number":"12345678899999","businessLicense":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","companyPhoto":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]"}})
      * @returnExample(true)
-     *
-     * @author fenghaikun<fenghaikun@eelly.net>
-     *
-     * @since 2017-8-02
+     * @throws \Eelly\SDK\Service\Exception\CompanyException
+     * @author wujunhua<wujunhua@eelly.net>
+     * @since 2017-09-04
      */
     public function addCompany(array $data, UidDTO $user = null): bool;
 
@@ -52,29 +63,24 @@ interface CompanyInterface
      * 修改店铺企业认证数据.
      * 用于用户修改认证信息.
      *
-     * @param array  $data                     认证数据
-     * @param int    $data['store_id']
-     * @param int    $data['name']
-     * @param int    $data['license']
-     * @param int    $data['mobile']
-     * @param int    $data['company']
-     * @param int    $data['number']
-     * @param int    $data['business_license']
-     * @param int    $data['company_photo']
-     * @param int    $data['status']
-     * @param UidDTO $user                     登录用户对象
-     *
-     * @throws Eelly\SDK\Service\Exception\CompanyException 701001 参数错误
-     * @throws Eelly\SDK\Service\Exception\CompanyException 700001 用户未登录
-     * @throws Eelly\SDK\Service\Exception\CompanyException 702001 数据不存在
+     * @param array  $data                    认证数据
+     * @param int    $data['storeId']         店铺id
+     * @param string $data['name']            真实姓名
+     * @param string $data['license']         身份证号码
+     * @param string $data['mobile']          手机号
+     * @param string $data['company']         企业名称
+     * @param string $data['number']          工商注册号
+     * @param string $data['businessLicense'] 营业执照图片地址，最多5张
+     * @param string $data['companyPhoto']    企业实拍图片地址，最多5张
+     * @param int    $data['status']          处理状态：0 未处理 1 审核通过 2 审核失败 3 认证过期
+     * @param UidDTO $user                    登录用户对象
      *
      * @return bool
-     * @requestExample({"data":{"store_id":1,"name":"\u5e97\u94fa1","license":"440981198806232871","mobile":"13427587735","company":"sixdec","number":"12345678899999","business_license":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","company_photo":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","status":1}})
+     * @requestExample({"data":{"storeId":1,"name":"\u5e97\u94fa1","license":"440981198806232871","mobile":"13427587735","company":"sixdec","number":"12345678899999","businessLicense":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","companyPhoto":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","status":1}})
      * @returnExample(true)
-     *
-     * @author fenghaikun<fenghaikun@eelly.net>
-     *
-     * @since 2017-8-02
+     * @throws \Eelly\SDK\Service\Exception\CompanyException
+     * @author wujunhua<wujunhua@eelly.net>
+     * @since 2017-09-04
      */
     public function updateCompany(array $data, UidDTO $user = null): bool;
 
@@ -85,36 +91,13 @@ interface CompanyInterface
      * @param int    $storeId 店铺ID
      * @param UidDTO $user    登录用户对象
      *
-     * @throws Eelly\SDK\Service\Exception\CompanyException 701001 参数错误
-     * @throws Eelly\SDK\Service\Exception\CompanyException 700001 用户未登录
-     * @throws Eelly\SDK\Service\Exception\CompanyException 700002 无权限操作
-     * @throws Eelly\SDK\Service\Exception\CompanyException 702001 数据不存在
-     *
      * @return bool
      * @requestExample({"storeId":1})
      * @returnExample(true)
-     *
-     * @author fenghaikun<fenghaikun@eelly.net>
-     *
-     * @since 2017-8-02
+     * @throws \Eelly\SDK\Service\Exception\CompanyException
+     * @author wujunhua<wujunhua@eelly.net>
+     * @since 2017-09-04
      */
     public function checkCompany(int $storeId, UidDTO $user = null): bool;
 
-    /**
-     * 获取一条店铺企业认证记录.
-     *
-     * @param int $storeId 店铺ID
-     *
-     * @throws Eelly\SDK\Service\Exception\CompanyException 701001 参数错误
-     * @throws Eelly\SDK\Service\Exception\CompanyException 702001 数据不存在
-     *
-     * @return array
-     * @requestExample({"storeId":1})
-     * @returnExample({"store_id":1,"name":"\u5e97\u94fa1","license":"440981198806232871","mobile":"13427587735","company":"sixdec","number":"12345678899999","business_license":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","company_photo":"[\"https:\\\/\\\/img03.eelly.com\\\/G04\\\/M00\\\/00\\\/98\\\/small_118_poYBAFjWXYaIWdWkAAJU9MtwDJgAAA7dgEx54sAAlUM236.jpg\",\"https:\\\/\\\/img03.eelly.com\\\/G02\\\/M00\\\/00\\\/BD\\\/small_118_ooYBAFjWXYqICR1yAAJWitxTD2AAABXRwMJp64AAlai106.jpg\"]","status":1,"created_time":1458093605})
-     *
-     * @author fenghaikun<fenghaikun@eelly.net>
-     *
-     * @since 2017-8-02
-     */
-    public function getCompany(int $storeId): array;
 }
