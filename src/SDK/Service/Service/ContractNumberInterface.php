fb1fe525247f16347732c1c8c4ed8daa9547ef92
wujunhua <330496248@qq.com>
2017-09-09 14:41:52 +0800

    新增合同编号相关，去除 interface的注解验证

--- src/SDK/Service/Service/ContractNumberInterface.php
+++ src/SDK/Service/Service/ContractNumberInterface.php
@@ -20,74 +20,66 @@ use Eelly\DTO\UidDTO;
  */
 interface ContractNumberInterface
 {
-    /**
-     * 新增合同编号.
-     *
-     * @param array  $data 新增数据
-     * @param UidDTO $user 登录用户对象
-     *
-     * @return array
-     * @requestExample()
-     * @returnExample()
-     *
-     * @author fenghaikun<fenghaikun@eelly.net>
-     *
-     * @since 2017-8-02
-     */
-    public function addContractNumber(array $data, UidDTO $user = null): array;
 
     /**
-     * 获取指定ID的合同编号.
+     * 新增合同编号
      *
-     * @param int $scnId 合同编号ID
+     * @param int    $scId   合同版本ID
+     * @param int    $number 新增合同编号数量
+     * @param UidDTO $user   登录用户对象
      *
-     * @return array
-     * @requestExample()
-     * @returnExample()
+     * @throws \Eelly\SDK\Service\Exception\ContractNumberException
      *
-     * @author fenghaikun<fenghaikun@eelly.net>
+     * @return bool 新增结果
+     * @requestExample({"versionNo":"合同版本号","number":10})
+     * @returnExample(true)
      *
-     * @since 2017-8-02
+     * @author wujunhua<wujunhua@eelly.net>
+     *
+     * @since 2017-09-08
      */
-    public function getContractNumber(int $scnId): array;
+    public function addContractNumber(int $scId, int $number, UidDTO $user = null): bool;
 
     /**
-     * 修改合同编号.
-     * 修改信息，包括状态
+     * 获取合同编号列表
      *
-     * @param array  $data list的数据
-     * @param UidDTO $user 登录用户对象
+     * @param array  $condition              查询条件
+     * @param string $condition['number']    合同编号
+     * @param string $condition['versionNo'] 版本号
+     * @param int    $condition['status']    编号状态
+     * @param int    $currentPage            当前页码
+     * @param int    $limit                  每页条数
+     * @param UidDTO $user                   登录用户对象
      *
-     * @return array
-     * @requestExample()
-     * @returnExample()
+     * @throws \Eelly\SDK\Service\Exception\ContractNumberException
      *
-     * @author fenghaikun<fenghaikun@eelly.net>
+     * @return array 列表信息
+     * @requestExample({"condition":{"number":"合同编号","versionNo":"版本号","status":0}})
+     * @returnExample({"item":[{"scnId":1,"number":"合同编号","status":0,"versionNo":1}],"page":{"first":1,"before":1,"current":1,"last":1,"next":1,"totalPages":1,"totalItems":1,"limit":1}})
      *
-     * @since 2017-8-02
+     * @author wujunhua<wujunhua@eelly.net>
+     *
+     * @since 2017-09-08
      */
-    public function updateContractNumber(array $data, UidDTO $user = null): array;
+    public function listContractNumberPage(array $condition = [], int $currentPage = 1, int $limit = 10, UidDTO $user = null): array;
 
     /**
-     * 获取合同编号列表.
+     * 解绑合同编号
      *
-     * @param array  $condition   查询条件
-     * @param int    $limit       每页页数
-     * @param int    $currentPage 当前页
-     * @param UidDTO $user        登录用户对象
+     * @param int    $scnId 合同编号id
+     * @param UidDTO $user  登录用户对象
      *
-     * @return array
-     * @requestExample()
-     * @returnExample()
+     * @throws \Eelly\SDK\Service\Exception\ContractNumberException
      *
-     * @author fenghaikun<fenghaikun@eelly.net>
+     * @return bool 解绑结果
+     * @requestExample({"scnId":1})
+     * @returnExample(true)
      *
-     * @since 2017-8-02
+     * @author wujunhua<wujunhua@eelly.net>
+     *
+     * @since 2017-09-08
      */
-
-    public function listContractNumberPage(array $condition = [],int $currentPage = 1, int $limit = 10,UidDTO $user=null):array;
-    
-
+    public function unbindContractNumber(int $scnId, UidDTO $user = null): bool;
 
 }
 
