14b2dae2173e18549375116bf0b6ea31a3ebebf7
laraveljun <xiaojunming@eelly.net>
2017-09-11 16:55:29 +0800

    log商品价格变更

--- src/SDK/Log/Service/GoodsPriceInterface.php
+++ src/SDK/Log/Service/GoodsPriceInterface.php
@@ -10,8 +10,8 @@ declare(strict_types=1);
  */
 
 namespace Eelly\SDK\Log\Service;
+use Eelly\SDK\Log\DTO\GoodsPriceDTO;
 
-use Eelly\DTO\GoodsPriceDTO;
 
 /**
  *
@@ -21,14 +21,36 @@ interface GoodsPriceInterface
 {
 
     /**
+     * 获取一条价格记录.
      *
-     * @author eellytools<localhost.shell@gmail.com>
+     * @param int $lgpId 历史记录ID
+     * @throws \Eelly\SDK\Log\Exception\GoodsHandleException
+     * @return GoodsPriceDTO
+     * @requestExample({'lgpId':1})
+     * @returnExample({"lgpId": 1, "goodsId": 1, "quantity": 2, "price": "2", "type": 3, "createdTime": "2017-09-04 16:07:05"})
+     * @author 肖俊明<xiaojunming@eelly.net>
+     * @since 2017年09月11日
+     * @Validation(
+     *      @OperatorValidator(0,{message : "日志ID",operator:["gt",0]})
+     * )
      */
-    public function getGoodsPrice(int $GoodsPriceId): GoodsPriceDTO;
+     public function getGoodsPrice(int $lgpId): GoodsPriceDTO;
 
     /**
+     * 记录当前的商品的价格.
      *
-     * @author eellytools<localhost.shell@gmail.com>
+     * @param array $data
+     * @param int    $data ['goodsId'] 商品ID
+     * @param int    $data ['quantity'] 区间起始数量
+     * @param int    $data ['price'] 商品价格
+     * @param int    $data ['type'] 价格类型：1 散批 2 拿货 3 打包
+     *
+     * @throws  \Eelly\SDK\Log\Exception\GoodsHandleException
+     * @return bool
+     * @requestExample({'data':{'goodsId':1,'quantity':1,'price':200,'type':1}})
+     * @returnExample(true)
+     * @author 肖俊明<xiaojunming@eelly.net>
+     * @since 2017年09月04日
      */
     public function addGoodsPrice(array $data): bool;
 
@@ -36,19 +58,30 @@ interface GoodsPriceInterface
      *
      * @author eellytools<localhost.shell@gmail.com>
      */
-    public function updateGoodsPrice(int $GoodsPriceId, array $data): bool;
+    //public function updateGoodsPrice(int $GoodsPriceId, array $data): bool;
 
     /**
      *
      * @author eellytools<localhost.shell@gmail.com>
      */
-    public function deleteGoodsPrice(int $GoodsPriceId): bool;
+    //public function deleteGoodsPrice(int $GoodsPriceId): bool;
 
     /**
+     * 商品价格分页操作.
      *
-     * @author eellytools<localhost.shell@gmail.com>
+     * @param int    $goodsId     商品id
+     * @param int    $currentPage 第几页
+     * @param int    $limit       条数
+     *
+     * @throws \Eelly\SDK\Log\Exception\GoodsHandleException
+     *
+     * @return array
+     * @requestExample({'goodsId':1,'currentPage':1,'limit':10})
+     * @returnExample(true)
+     * @author 肖俊明<xiaojunming@eelly.net>
+     * @since 2017年09月04日
      */
-    public function listGoodsPricePage(array $condition = [], int $limit = 10, int $currentPage = 1): array;
+    public function listGoodsPricePage(int $goodsId, int $currentPage = 1, int $limit = 10):array;
 
 
 }
