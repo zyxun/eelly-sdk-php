779594a4fe493b1cf8d9fcc7a4652c44a705c040
laraveljun <xiaojunming@eelly.net>
2017-09-15 19:02:31 +0800

    支付模块

--- src/SDK/Pay/Service/ApplyBankInterface.php
+++ src/SDK/Pay/Service/ApplyBankInterface.php
@@ -1,21 +1,54 @@
 <?php
 
 declare(strict_types=1);
-
 /*
- * This file is part of eelly package.
+ * PHP version 7.1
  *
- * (c) eelly.com
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @copyright Copyright (c) 2012-2017 EELLY Inc. (https://www.eelly.com)
+ * @link      https://api.eelly.com
+ * @license   衣联网版权所有
  */
 
 namespace Eelly\SDK\Pay\Service;
 
+use Eelly\DTO\ApplyBankDTO;
+
 /**
+ *
  * @author eellytools<localhost.shell@gmail.com>
  */
 interface ApplyBankInterface
 {
+
+    /**
+     *
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function getApplyBank(int $applyBankId): ApplyBankDTO;
+
+    /**
+     *
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function addApplyBank(array $data): bool;
+
+    /**
+     *
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function updateApplyBank(int $applyBankId, array $data): bool;
+
+    /**
+     *
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function deleteApplyBank(int $applyBankId): bool;
+
+    /**
+     *
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function listApplyBankPage(array $condition = [], int $currentPage = 1, int $limit = 10): array;
+
+
 }
