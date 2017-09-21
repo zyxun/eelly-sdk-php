2d794a80dcf9ff981d87280e7e619e0a6f7dbf14
wujunhua <330496248@qq.com>
2017-09-21 15:55:39 +0800

    service模块 新增增值服务相关

--- src/SDK/Service/Api/Service.php
+++ src/SDK/Service/Api/Service.php
@@ -13,6 +13,9 @@ declare(strict_types=1);
 
 namespace Eelly\SDK\Service\Api;
 
+use Eelly\DTO\UidDTO;
+use Eelly\SDK\EellyClient;
+use Eelly\SDK\Service\DTO\ServiceDTO;
 use Eelly\SDK\Service\Service\ServiceInterface;
 
 /**
@@ -20,6 +23,55 @@ use Eelly\SDK\Service\Service\ServiceInterface;
  */
 class Service implements ServiceInterface
 {
+
+    /**
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function getService(int $serviceId): ServiceDTO
+    {
+        return EellyClient::request('service/Service', 'getService', $serviceId);
+    }
+
+    /**
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function addService(array $data, UidDTO $user = null): bool
+    {
+        return EellyClient::request('service/Service', 'addService', $data, $user);
+    }
+
+    /**
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function updateService(int $serviceId, array $data, UidDTO $user = null): bool
+    {
+        return EellyClient::request('service/Service', 'updateService', $serviceId, $data, $user);
+    }
+
+    /**
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function listServicePage(array $condition = [], int $currentPage = 1, int $limit = 10): array
+    {
+        return EellyClient::request('service/Service', 'listServicePage', $condition, $currentPage, $limit);
+    }
+
+    /**
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function listBuyerServicePage(array $condition = [], int $currentPage = 1, int $limit = 10): array
+    {
+        return EellyClient::request('service/Service', 'listServicePage', $condition, $currentPage, $limit);
+    }
+
+    /**
+     * @author eellytools<localhost.shell@gmail.com>
+     */
+    public function listSellerServicePage(array $condition = [], int $currentPage = 1, int $limit = 10): array
+    {
+        return EellyClient::request('service/Service', 'listServicePage', $condition, $currentPage, $limit);
+    }
+
     /**
      * @return self
      */
