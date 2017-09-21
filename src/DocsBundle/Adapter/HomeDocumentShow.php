9558cb5146e30119ec26f5bd15db9f60f10f1909
runphp <runphp@qq.com>
2017-09-21 10:58:42 +0800

    首页内容移到模板中

--- src/DocsBundle/Adapter/HomeDocumentShow.php
+++ src/DocsBundle/Adapter/HomeDocumentShow.php
@@ -20,25 +20,7 @@ class HomeDocumentShow extends AbstractDocumentShow implements DocumentShowInter
 {
     public function renderBody(): void
     {
-        $markdown = <<<EOF
-# 衣联网api开放文档
-
-> 本文档由接口代码文档生成，接口文档使用[github markdown](https://guides.github.com/features/mastering-markdown/)风格
-
-> 贡献接口的phper请完善好接口文档
-
-> 特殊注解说明(`@requestExample`表示请求示例，`@returnExample`表示返回数据示例)
-
-### 接口定义示例
-
-> 源文件: [UserInterface.php](https://raw.githubusercontent.com/EellyDev/eelly-sdk-php/master/src/SDK/User/Service/UserInterface.php)
-
-> 生成的文档: [user service doc](/user/user)
-
-更详细的说明将在[sdk-php-wiki](https://github.com/EellyDev/eelly-sdk-php/wiki)进行说明
-
-EOF;
-        $this->view->markup = $this->parserMarkdown($markdown);
+        $this->view->markup = function ($markdown) {return $this->parserMarkdown($markdown); };
         $this->view->render('apidoc', 'home');
     }
 }
