3eacac7e9b6b63e7aa186af912b139dd6cfaf56d
zhangzeqiang <zhangzeqiang@eelly.net>
2017-11-10 09:31:47 +0800

    支付模块

--- src/SDK/Pay/Service/CallbackInterface.php
+++ src/SDK/Pay/Service/CallbackInterface.php
@@ -27,28 +27,18 @@ interface CallbackInterface
     public function getCallback(int $callbackId): CallbackDTO;
 
     /**
+     * 添加 第三方支付回调记录
+     * @param array $data
      *
-     * @author eellytools<localhost.shell@gmail.com>
+     * @throws LogicException
+     *
+     * @return bool
+     * @requestExample({"data":{"billNo":"001","channel":1,"money":100,"content":"测试写入","remark":""}})
+     * @returnExample(true)
+     *
+     * @author zhangzeqiang<zhangzeqiang@eelly.net>
+     * @since  2017年11月9日
      */
     public function addCallback(array $data): bool;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
-    public function updateCallback(int $callbackId, array $data): bool;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
-    public function deleteCallback(int $callbackId): bool;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
-    public function listCallbackPage(array $condition = [], int $currentPage = 1, int $limit = 10): array;
-
 
 }
--- src/SDK/Pay/Service/RequestInterface.php
+++ src/SDK/Pay/Service/RequestInterface.php
@@ -24,31 +24,22 @@ interface RequestInterface
      *
      * @author eellytools<localhost.shell@gmail.com>
      */
-    public function getRequest(int $requestId): RequestDTO;
+    public function getRequest(string $requestId): RequestDTO;
 
     /**
+     * 添加 发起第三方支付数据记录
+     * @param array $data
      *
-     * @author eellytools<localhost.shell@gmail.com>
+     * @throws LogicException
+     *
+     * @return bool
+     * @requestExample({"data":{"billNo":"001","channel":1,"money":100,"content":"第三方支付请求前测试写入","remark":""}})
+     * @returnExample(true)
+     *
+     * @author zhangzeqiang<zhangzeqiang@eelly.net>
+     * @since  2017年11月9日
      */
     public function addRequest(array $data): bool;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
-    public function updateRequest(int $requestId, array $data): bool;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
-    public function deleteRequest(int $requestId): bool;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
-    public function listRequestPage(array $condition = [], int $currentPage = 1, int $limit = 10): array;
 
 
 }
