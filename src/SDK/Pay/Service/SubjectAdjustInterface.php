041af7cf001a12579908e1a024a0babb0c5e6b10
zhangyingdi <568321490@qq.com>
2017-11-21 14:08:07 +0800

    支付模块，核算代码提交

--- src/SDK/Pay/Service/SubjectAdjustInterface.php
+++ src/SDK/Pay/Service/SubjectAdjustInterface.php
@@ -23,46 +23,30 @@ interface SubjectAdjustInterface
     /**
      * 根据结算日期跟科目代码，获取会计科目日核算记录
      *
-     * @param string $workDate 结算日期：格式YYYYMMDD
      * @param string $subjectCode 科目代码
+     * @param string $workDate 结算日期：格式YYYYMMDD
      * @return SubjectAdjustDTO
      *
-     * @author zhangyingdi<zhangyingdi@eelly.net>
-     * @since  2017-11-09
-     */
-    public function getSubjectAdjust(string $workDate, string $subjectCode): SubjectAdjustDTO;
-
-    /**
-     * 添加一条会计科目日核算记录
+     * @requestExample({"workDate":"20171109","subjectCode":"eellyPay"})
+     * @returnExample({"workDate":"20171109","subjectCode":"eellyPay","subjectMoney":20,"accountMoney":10,"balanceStatus":1,"remark":"","createdTime":135234565})
      *
-     * @param array $data 会计科目日核算记录数据
-     * @param string $data['workDate'] 结算日期：格式YYYYMMDD
-     * @param string $data['subjectCode'] 科目代码
-     * @param int $data['subjectMoney'] 科目资金发生额：科目贷方金额减去借方金额
-     * @param int $data['accountMoney'] 帐户变动流水发生额
-     * @param int $data['balanceStatus'] 比较结果：0 未比较 1 平衡 2 不平衡
-     * @param string $data['remark'] 备注
+     * @author zhangyingdi<zhangyingdi@eelly.net>
+     * @since  2017-11-20
+     */
+    public function getSubjectAdjust(string $subjectCode, string $workDate): SubjectAdjustDTO;
+    
+    /**
+     * 自动脚本添加科目日核算记录
+     *
+     * @param string $date 结算日期：格式YYYYMMDD
      * @return bool
      *
-     * @requestExample({"data":{"workDate":"20171109","subjectCode":"dd","subjectMoney":20,"accountMoney":10,"balanceStatus":1,"remark":""}})
+     * @requestExample({"data":{"workDate":"20171109","subjectCode":"eellyPay","subjectMoney":20,"accountMoney":10,"balanceStatus":1,"remark":"","createdTime":135234565}})
      * @returnExample(true)
      *
      * @author zhangyingdi<zhangyingdi@gmail.com>
-     * @since  2017-11-09
+     * @since  2017-11-21
      */
-    public function addSubjectAdjust(array $data): bool;
+    public function autoRunSubjectAdjust(string $date): bool;
     
-    /**
-     *
-     * @author zhangyingdi<zhangyingdi@eelly.net>
-     */
-    public function deleteSubjectAdjust(int $subjectAdjustId): bool;
-
-    /**
-     *
-     * @author zhangyingdi<zhangyingdi@eelly.net>
-     */
-    public function listSubjectAdjustPage(array $condition = [], int $currentPage = 1, int $limit = 10): array;
-
-
 }
