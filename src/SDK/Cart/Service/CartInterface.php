2dc990ce458569f4b721d31b9673c0c1c14176a9
laraveljun <xiaojunming@eelly.net>
2017-12-26 15:36:09 +0800

    购物车改造

--- src/SDK/Cart/Service/CartInterface.php
+++ src/SDK/Cart/Service/CartInterface.php
@@ -25,23 +25,33 @@ interface CartInterface
     /**
      * 添加进购物车.
      *
-     * @param int    $goodsId  商品id
-     * @param int    $spId     商品规格id
-     * @param int    $quantity 商品数量
-     * @param int    $price    商品价格，单位为分
-     * @param UidDTO $user     用户信息
+     * @param int    $goodsId    商品id
+     * @param int    $quantity   商品数量
+     * @param int    $price      商品价格
+     * @param array  $attributes 额外参数
+     * @param int    $attributes ["spId"] 商品规格id
+     * @param array  $attributes ["color"] 颜色
+     * @param array  $attributes ["size"]  码数
+     * @param UidDTO $user       用户信息
      *
      * @throws \Eelly\SDK\Cart\Exception\CartException
      *
      * @return bool 返回bool值
-     * @requestExample([1,2,1,2,10000,{"user_id":"1","username":"liangxinyi"}])
+     * @requestExample({"goodsId":1,"quantity":1,"price":10000})
      * @returnExample(true)
      *
      * @author liangxinyi<liangxinyi@eelly.net>
+     * @author 肖俊明<xiaojunming@eelly.net>
      *
-     * @since 2017-7-31
+     * @since 2017年12月25日
+     *
+     * @Validation(
+     *  @OperatorValidator(0,{message:"商品ID必须大于0",operator:["gt",0]}),
+     *  @OperatorValidator(1,{message:"商品数量必须大于0",operator:["gt",0]}),
+     *  @OperatorValidator(2,{message:"商品价格必须大于0",operator:["gt",0]})
+     *)
      */
-    public function addCart(int $goodsId, int $spId, int $quantity, int $price, UidDTO $user = null): bool;
+    public function addCart(int $goodsId, int $quantity, int $price, array $attributes = [], UidDTO $user = null): bool;
 
     /**
      * 清空购物车.
@@ -50,11 +60,12 @@ interface CartInterface
      *
      * @throws \Eelly\SDK\Cart\Exception\CartException
      *
-     * @return bool
-     * @requestExample({"user_id":"1","username":"liangxinyi"})
+     * @return bool 返回bool值
+     * @requestExample()
      * @returnExample(true)
      *
      * @author liangxinyi<liangxinyi@eelly.net>
+     * @author 肖俊明<xiaojunming@eelly.net>
      *
      * @since 2017-7-31
      */
@@ -65,11 +76,12 @@ interface CartInterface
      *
      * @param UidDTO $user 用户信息
      *
-     * @return array
-     * @requestExample({"user_id":"1","username":"liangxinyi"})
-     * @returnExample({"goods_3_2":"{\"goods_id\":3,\"sp_id\":2,\"quantity\":3,\"price\":10000,\"created_time\":1501517584,\"update_time\":0}","goods_1_2":"{\"goods_id\":1,\"sp_id\":2,\"quantity\":12,\"price\":10000,\"created_time\":1501515811,\"update_time\":\"2017-07-31 23:43:38\"}"})
+     * @return array 个人购物车列表
+     * @requestExample()
+     * @returnExample({{"rawId":"86e69d4b3503164e1de220c61e860d40","goodsId":11,"quantity":1,"price":11,"size":"大码","color":"红色"},{"rawId":"9b38f2c4f4cb25879e326e66f2ce0ae4","goodsId":11,"quantity":1,"price":11,"size":"大码","color":"红色"}})
      *
      * @author liangxinyi<liangxinyi@eelly.net>
+     * @author 肖俊明<xiaojunming@eelly.net>
      *
      * @since 2017-7-31
      */
@@ -78,59 +90,70 @@ interface CartInterface
     /**
      * 删除指定id购物车数据.
      *
-     * @param int string 指定购物车key值，数据格式$cartId='goods_1_2'
-     * @param int    $spId 商品规格id
+     *
+     * @param string $rawId 指定购物车key值，goods+商品id+规格id,数据格式中md5值
      * @param UidDTO $user 用户信息
      *
      * @throws \InvalidArgumentException
      * @throws \Eelly\SDK\Cart\Exception\CartException
      *
      * @return bool
-     * @requestExample(['goods_1_2',{"user_id":"1","username":"liangxinyi"}])
+     * @requestExample({"rawId":"86e69d4b3503164e1de220c61e860d40"})
      * @returnExample(true)
      *
      * @author liangxinyi<liangxinyi@eelly.net>
+     * @author 肖俊明<xiaojunming@eelly.net>
      *
-     * @since 2017-7-31
+     * @since 2017年12月26日
+     * @Validation(
+     *     @PresenceOf(0,{message :"非法购物车key值"})
+     * )
      */
-    public function deleteCart(string $cartId, UidDTO $user): bool;
+    public function deleteCart(string $rawId, UidDTO $user): bool;
 
     /**
      * 更新指定id购物车.
      *
-     * @param string $cartId   指定购物车key值，数据格式$cartId='goods_1_2'
-     * @param int    $quantity 商品数量
-     * @param UidDTO $user     用户信息
      *
-     * @throws \InvalidArgumentException
+     * @param string $rawId 指定购物车key值
+     * @param array  $data  修改数据
+     * @param UidDTO $user  用户信息
+     *
      * @throws \Eelly\SDK\Cart\Exception\CartException
      *
      * @return bool
-     * @requestExample(['goods_1_2,3',{"user_id":"1","username":"liangxinyi"}])
+     * @requestExample({"rawId":"86e69d4b3503164e1de220c61e860d40","data":{"quantity":2,"price":120}})
      * @returnExample(true)
      *
      * @author liangxinyi<liangxinyi@eelly.net>
+     * @author 肖俊明<xiaojunming@eelly.net>
      *
      * @since 2017-7-31
+     * @Validation(
+     *     @PresenceOf(0,{message :"非法购物车key值"})
+     * )
      */
-    public function updateCart(string $cartId, int $quantity, UidDTO $user): bool;
+    public function updateCart(string $rawId, array $data = [], UidDTO $user = null): bool;
 
     /**
      * 批量移除购物车.
      *
-     * @param array  $cartIds 购物车key值id数组,数据格式$cartIds=[0=>'goods_1_2']
-     * @param UidDTO $user    用户信息
+     * @param array  $rawIds 购物车key值id数组,数据格式$cartIds=[0=>'goods_1_2']
+     * @param UidDTO $user   用户信息
      *
-     * @throws \InvalidArgumentException
      * @throws \Eelly\SDK\Cart\Exception\CartException
      *
-     * @return bool
-     * @requestExample([[1,2,3],{"user_id":"1","username":"liangxinyi"}])
+     * @return bool 返回bool值
+     * @requestExample({"rawIds":{"86e69d4b3503164e1de220c61e860d40","9b38f2c4f4cb25879e326e66f2ce0ae4"}})
      * @returnExample(true)
      *
      * @author liangxinyi<liangxinyi@eelly.net>
+     * @author 肖俊明<xiaojunming@eelly.net>
      *
-     * @since 2017-7-31
+     * @since 2017年12月26日
+     * @Validation(
+     *     @PresenceOf(0,{message :"非法购物车key值"})
+     * )
      */
-    public function deleteCartBatch(array $cartIds, UidDTO $user): bool;
+    public function deleteCartBatch(array $rawIds, UidDTO $user): bool;
 }
