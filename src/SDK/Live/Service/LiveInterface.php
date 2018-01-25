ca8d4c8c5cde69c413b066a83892fb13168d1236
runphp <runphp@qq.com>
2018-01-25 17:50:21 +0800

    add getStoreLive

--- src/SDK/Live/Service/LiveInterface.php
+++ src/SDK/Live/Service/LiveInterface.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 /*
  * This file is part of eelly package.
  *
@@ -15,7 +16,6 @@ namespace Eelly\SDK\Live\Service;
 use Eelly\SDK\Live\DTO\LiveDTO;
 
 /**
- *
  * @author shadonTools<localhost.shell@gmail.com>
  */
 interface LiveInterface
@@ -34,30 +34,27 @@ interface LiveInterface
      *     "share":1,"scheduleTime":1503560249,"startTime":1503560249,"endTime":1503560249,"status":1,"sort":1,"createdTime":1503560249})
      *
      * @author zhangyingdi<zhangyingdi@eelly.net>
+     *
      * @since  2018.01.24
      */
     public function getLive(int $liveId): LiveDTO;
 
     /**
-     *
      * @author eellytools<localhost.shell@gmail.com>
      */
     public function addLive(array $data): bool;
 
     /**
-     *
      * @author eellytools<localhost.shell@gmail.com>
      */
     public function updateLive(int $liveId, array $data): bool;
 
     /**
-     *
      * @author eellytools<localhost.shell@gmail.com>
      */
     public function deleteLive(int $liveId): bool;
 
     /**
-     *
      * @author eellytools<localhost.shell@gmail.com>
      */
     public function listLivePage(array $condition = [], int $currentPage = 1, int $limit = 10): array;
@@ -76,19 +73,22 @@ interface LiveInterface
      * finished |int    |已结束数量
      *
      *
-     * @param array $condition 条件
-     * @param string $field 查询的key值
+     * @param array  $condition 条件
+     * @param string $field     查询的key值
+     *
      * @return array
      * @requestExample({"field":"getStat"})
      * @returnExample({"total":2,"pending":0,"occurred":1,"progress":1,"finished":0})
+     *
      * @author 肖俊明<xiaojunming@eelly.net>
+     *
      * @since 2018年01月23日
      */
     public function getStatistics(array $condition = [], string $field = 'getStat'): array;
 
     /**
      * 厂+直播管理列表.
-     * ### 返回数据说明
+     * ### 返回数据说明.
      *
      * 字段|类型|说明
      * ----------------------|-------|--------------
@@ -111,13 +111,16 @@ interface LiveInterface
      * page["current"]       |int    |当前页
      * page["limit"]         |int    |每页条数
      *
-     * @param array $condition 查询条件
-     * @param int $currentPage 第几页
-     * @param int $limit 每页条数
+     * @param array $condition   查询条件
+     * @param int   $currentPage 第几页
+     * @param int   $limit       每页条数
+     *
      * @return array
      * @requestExample({"condition":["storeIds":[148086,148087],"inStatus":[0, 1, 12, 13]],"currentPage":1,"limit":10,"order":"base"})
      * @returnExample({"items":[{"liveId":1,"userId":2,"storeId":148086,"title":"11","image":"1111","scheduleTime":1516353883,"startTime":0,"endTime":0,"sort":4,"status":0,"createdTime":1516204800,"goodsCount":2}],"page":{"totalPages":1,"totalItems":2,"current":1,"limit":10}})
+     *
      * @author 肖俊明<xiaojunming@eelly.net>
+     *
      * @since 2018年01月23日
      * @Validation(
      *  @PresenceOf(0,{message : "数据不能为空"}),
@@ -129,7 +132,7 @@ interface LiveInterface
 
     /**
      * 厂+直播管理列表.
-     * ### 返回数据说明
+     * ### 返回数据说明.
      *
      * 字段|类型|说明
      * ----------------------|-------|--------------
@@ -153,16 +156,19 @@ interface LiveInterface
      * page["current"]       |int    |当前页
      * page["limit"]         |int    |每页条数
      *
-     * @param array $condition 查询条件
-     * @param array $condition['storeIds'] 店铺ID一维数据
-     * @param array $condition['inStatus'] 查询状态
-     * @param int $condition['lastSchedule'] 最后的播放日期,第一页不用传递，或者0
-     * @param int $currentPage 第几页
-     * @param int $limit 每页条数
+     * @param array $condition                 查询条件
+     * @param array $condition['storeIds']     店铺ID一维数据
+     * @param array $condition['inStatus']     查询状态
+     * @param int   $condition['lastSchedule'] 最后的播放日期,第一页不用传递，或者0
+     * @param int   $currentPage               第几页
+     * @param int   $limit                     每页条数
+     *
      * @return array
      * @requestExample({"condition":["storeIds":[148086,148087],"inStatus":[1, 12, 13],"lastSchedule":"1516353883"],"currentPage":1,"limit":10,"order":"base"})
      * @returnExample({"items":[{"liveId":1,"userId":2,"storeId":148086,"title":"11","image":"1111","scheduleTime":1516353883,"startTime":0,"endTime":0,"sort":4,"status":0,"createdTime":1516204800,"goodsCount":2,"goodsList":[1,2,3]}],"page":{"totalPages":1,"totalItems":2,"current":1,"limit":10}})
+     *
      * @author 肖俊明<xiaojunming@eelly.net>
+     *
      * @since 2018年01月23日
      * @Validation(
      *  @PresenceOf(0,{message : "数据不能为空"}),
@@ -175,17 +181,20 @@ interface LiveInterface
     /**
      * 直播管理列表.
      *
-     * @param array $condition 查询条件
-     * @param array $condition['storeIds'] 店铺ID一维数据
-     * @param array $condition['inStatus'] 查询状态
-     * @param int $condition['scheduleDate'] 开播日期
-     * @param int $currentPage 第几页
-     * @param int $limit 每页条数
-     * @param string $order 排序
+     * @param array  $condition                 查询条件
+     * @param array  $condition['storeIds']     店铺ID一维数据
+     * @param array  $condition['inStatus']     查询状态
+     * @param int    $condition['scheduleDate'] 开播日期
+     * @param int    $currentPage               第几页
+     * @param int    $limit                     每页条数
+     * @param string $order                     排序
+     *
      * @return array
      * @requestExample({"condition":["storeIds":[148086,148087],"inStatus":[0,1],"scheduleDate":"1516291200"],"currentPage":1,"limit":10,"order":"base"})
      * @returnExample({"items":[{"liveId":1,"userId":2,"buyerId":148086,"storeId":148086,"title":"11","image":"1111","scheduleTime":1516353883,"sort":255,"status":0,"createdTime":1516204800,"userCount":2}],"page":{"totalPages":1,"totalItems":2,"current":1,"limit":10}})
+     *
      * @author 肖俊明<xiaojunming@eelly.net>
+     *
      * @since 2018年01月22日
      * @Validation(
      *  @PresenceOf(0,{message : "数据不能为空"}),
@@ -196,11 +205,12 @@ interface LiveInterface
     public function getLiveList(array $condition, int $currentPage = 1, int $limit = 10, string $order = 'base'): array;
 
     /**
-     * 根据传过来的条件返回对应的数据
+     * 根据传过来的条件返回对应的数据.
      *
-     * @param array $condition 传递参数
-     * @param string $field  字段
-     * @param string $order  排序
+     * @param array  $condition 传递参数
+     * @param string $field     字段
+     * @param string $order     排序
+     *
      * @return array
      *
      * @requestExample({
@@ -214,32 +224,34 @@ interface LiveInterface
      * })
      *
      * @author zhangyingdi<zhangyingdi@eelly.net>
+     *
      * @since 2018.01.22
      */
     public function getLiveListByCondition(array $condition, string $field = 'base', string $order = 'base');
-     /*
-     * 批量更新排序.
-     *
-     * @param array $sort [1=>4,2=>8]
-     * @return bool
-     * @requestExample({"sort":["1":4,"2":8]})
-     * @returnExample(true)
-     * @author 肖俊明<xiaojunming@eelly.net>
-     * @since 2018年01月23日
-     * @Validation(
-     *  @PresenceOf(0,{message : "数据不能为空"})
-     *)
-     */
+
+    /*
+    * 批量更新排序.
+    *
+    * @param array $sort [1=>4,2=>8]
+    * @return bool
+    * @requestExample({"sort":["1":4,"2":8]})
+    * @returnExample(true)
+    * @author 肖俊明<xiaojunming@eelly.net>
+    * @since 2018年01月23日
+    * @Validation(
+    *  @PresenceOf(0,{message : "数据不能为空"})
+    *)
+    */
     public function updateSort(array $sort): bool;
-    
+
     /**
-     * 支付成功后插入空直播信息
-     * 
-     * @param array $data 请求参数
-     * @param int $data['count'] 场数
-     * @param int $data['userId'] 用户ID
-     * @param int $data['storeId'] 店铺ID
-     * 
+     * 支付成功后插入空直播信息.
+     *
+     * @param array $data            请求参数
+     * @param int   $data['count']   场数
+     * @param int   $data['userId']  用户ID
+     * @param int   $data['storeId'] 店铺ID
+     *
      * @requestExample({
         "data": {
             "count": 1,
@@ -248,10 +260,23 @@ interface LiveInterface
         }
      })
      * @returnExample(true)
+     *
      * @return bool
-     * 
+     *
      * @author wechan<liweiquan@eelly.net>
+     *
      * @since 2018年01月23日
      */
     public function addReadyLive(array $data): bool;
+
+    /**
+     * 获取店铺的直播.
+     *
+     * @param int $storeId
+     *
+     * @return array
+     *
+     * @author hehui<hehui@eelly.net>
+     */
+    public function getStoreLive(int $storeId): array;
 }
