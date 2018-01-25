172230a32ace4ff7d2973efb0098525890c8e388
andy <568321490@qq.com>
2018-01-25 10:20:47 +0800

    衣联直播代码

---
+++ src/SDK/Live/DTO/LiveDTO.php
@@ -0,0 +1,123 @@
+<?php
+declare(strict_types=1);
+/**
+ * PHP version 5.5
+ *
+ * @copyright  Copyright (c) 2012-2015 EELLY Inc. (http://www.eelly.com)
+ * @link       http://www.eelly.com
+ * @license    衣联网版权所有
+ */
+
+
+namespace Eelly\SDK\Live\DTO;
+
+
+use Eelly\DTO\AbstractDTO;
+
+/**
+ * PayAccountDTO.
+ *
+ * @author zhangyingdi<zhangyingdi@eelly.net>
+ * @since  2018.01.24
+ */
+class LiveDTO extends AbstractDTO
+{
+    /**
+     * 直播信息，自增主键
+     *
+     * @var int
+     */
+    public $liveId;
+
+    /**
+     * 用户ID
+     *
+     * @var int
+     */
+    public $userId;
+
+    /**
+     * 店铺ID
+     *
+     * @var int
+     */
+    public $storeId;
+
+    /**
+     * 直播标题
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * 直播封面
+     *
+     * @var string
+     */
+    public $image;
+
+    /**
+     * 地区
+     *
+     * @var int
+     */
+    public $region;
+
+    /**
+     * 推流URL：用户前端播放引用
+     *
+     * @var string
+     */
+    public $pushUrl;
+
+    /**
+     * 分享渠道：1 微信 2 朋友圈 4 QQ好友 8 QQ空间
+     *
+     * @var int
+     */
+    public $share;
+
+    /**
+     * 预计视频开始时间
+     *
+     * @var int
+     */
+    public $scheduleTime;
+
+    /**
+    * 视频开始时间
+    *
+    * @var int
+    */
+    public $startTime;
+
+    /**
+     * 视频结束时间
+     *
+     * @var int
+     */
+    public $endTime;
+
+    /**
+     * 状态：0.直播未开始,1.直播中-进行中,12.直播中-暂停,13.直播中-异常暂停,2.正常结束,3.强制中止
+     *
+     * @var int
+     */
+    public $status;
+
+    /**
+     * 排序
+     *
+     * @var int
+     */
+    public $sort;
+
+    /**
+     * 添加时间
+     *
+     * @var int
+     */
+    public $createdTime;
+
+}
--- src/SDK/Live/Service/SubscribeInterface.php
+++ src/SDK/Live/Service/SubscribeInterface.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
 
 namespace Eelly\SDK\Live\Service;
 
-use \SDK\Live\DTO\SubscribeDTO;
-
 /**
  *
  * @author shadonTools<localhost.shell@gmail.com>
@@ -24,31 +22,19 @@ interface SubscribeInterface
      *
      * @author eellytools<localhost.shell@gmail.com>
      */
-    public function getSubscribe(int $subscribeId): SubscribeDTO;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
-    public function addSubscribe(array $data): bool;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
-    public function updateSubscribe(int $subscribeId, array $data): bool;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
-    public function deleteSubscribe(int $subscribeId): bool;
-
-    /**
-     *
-     * @author eellytools<localhost.shell@gmail.com>
-     */
     public function listSubscribePage(array $condition = [], int $currentPage = 1, int $limit = 10): array;
 
-
+    /**
+     * 根据传过来的查询条件，返回对应的数据
+     *
+     * @param array $condition 查询的where条件
+     * @return array
+     *
+     * @requestExample({"condition":{"liveId":1}})
+     * @returnExample([{"lsId": 1, "liveId": 1, "userId": 148086, "createdTime": 0}])
+     *
+     * @author zhangyingdi<zhangyingdi@eelly.net>
+     * @since 2018.01.25
+     */
+    public function getSubscribeList(array $condition = []): array;
 }
