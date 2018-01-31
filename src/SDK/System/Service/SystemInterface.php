b3cbdbe817d2c65ac719e594303e6526a6af3915
andy <568321490@qq.com>
2018-01-31 11:06:37 +0800

    获取系统相关信息

---
+++ src/SDK/System/Service/SystemInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\System\Service;
+
+
+/**
+ * 系统相关信息
+ *
+ * @author zhangyingdi<zhangyingdi@eelly.net>
+ */
+interface SystemInterface
+{
+    /**
+     * 获取当前系统时间
+     *
+     * @author zhangyingdi<zhangyingdi@eelly.net>
+     * @since   2018.01.31
+     */
+    public function getSystemTime(): int;
+
+}
