c913d38835e29723a5a30c7eb15748d85219a563
runphp <runphp@qq.com>
2018-04-26 16:49:50 +0800

    add method myAppletOrders

--- src/SDK/Order/Service/SellerOrderInterface.php
+++ src/SDK/Order/Service/SellerOrderInterface.php
@@ -20,4 +20,113 @@ namespace Eelly\SDK\Order\Service;
  */
 interface SellerOrderInterface
 {
+
+    /**
+     * 我的小程序订单.
+     *
+     * > 返回数据说明
+     *
+     * key         | type    | value
+     * ------------ |------  | --------
+     * current      | int    | 当前页码
+     * last         | int    | 下一页页码
+     * totalPages   | int    | 总页码
+     * totalItems   | int    | 数据总量
+     * items        | array  |当前数据
+     * items[]['orderId']     | string | 订单id
+     * items[]['sellerName']  | string | 店铺名
+     * items[]['orderStatus'] | int    | 订单状态
+     * items[]['orderAmount'] | float | 实付
+     * items[]['freight']     | float | 运费
+     * items[]['createdDate'] | date | 订单日期
+     * items[]['goodsList']   | array | 商品列表
+     * items[]['goodsList'][]['goodsName']    | string | 商品名称
+     * items[]['goodsList'][]['price']        | float | 商品价格
+     * items[]['goodsList'][]['quantity']     | float | 商品数量
+     * items[]['goodsList'][]['spec']         | string | 商品属性
+     * items[]['goodsList'][]['goodsImage']   | string | 商品图片
+     *
+     * @param int    $tab    订单筛选值  (0: 全部, 1: 待付款, 2: 待成团, 3: 待发货, 4: 待收货, 5: 待评价)
+     * @param int    $page   第几页
+     * @param int    $limit  分页大小
+     * @param UidDTO $uidDTO uid dto
+     * @return array
+     *
+     * @author hehui<hehui@eelly.net>
+     *
+     * @requestExample({"tab":1, "page":2})
+     *
+     * @returnExample(
+     * {
+     *     "first": 1,
+     *     "before": 1,
+     *     "current": 1,
+     *     "last": 23,
+     *     "next": 2,
+     *     "totalPages": 23,
+     *     "totalItems": 45,
+     *     "limit": 2,
+     *     "items": [
+     *         {
+     *             "orderId": "160",
+     *             "sellerName": "莫琼小店",
+     *             "osId": "26",
+     *             "likes": 2,
+     *             "evaluation": null,
+     *             "orderAmount": "2",
+     *             "freight": "1",
+     *             "createdTime": 1524555994,
+     *             "orderStatus": 8,
+     *             "createdDate": "2018-04-24",
+     *             "goodsList": [
+     *                 {
+     *                     "ogId": "20000215",
+     *                     "orderId": "160",
+     *                     "goodsId": "1450168293",
+     *                     "gsId": "195022196",
+     *                     "price": "1",
+     *                     "quantity": "2",
+     *                     "goodsName": "【莫琼小店】 2018新款 针织衫\/毛衣  包邮",
+     *                     "goodsImage": "https:\/\/img03.eelly.test\/G02\/M00\/00\/03\/small_ooYBAFqzVV2ICEGRAAER2psay8IAAABggBWRl0AARHy759.jpg",
+     *                     "goodsNumber": "2",
+     *                     "spec": "颜色:如图色,尺码:均码",
+     *                     "createdTime": "1524555994",
+     *                     "updateTime": "2018-04-24 15:46:32"
+     *                 }
+     *             ]
+     *         },
+     *         {
+     *             "orderId": "159",
+     *             "sellerName": "莫琼小店",
+     *             "osId": "26",
+     *             "likes": 0,
+     *             "evaluation": null,
+     *             "orderAmount": "2",
+     *             "freight": "1",
+     *             "createdTime": 1524550065,
+     *             "orderStatus": 7,
+     *             "createdDate": "2018-04-24",
+     *             "goodsList": [
+     *                 {
+     *                     "ogId": "20000214",
+     *                     "orderId": "159",
+     *                     "goodsId": "1450168293",
+     *                     "gsId": "195022196",
+     *                     "price": "1",
+     *                     "quantity": "2",
+     *                     "goodsName": "【莫琼小店】 2018新款 针织衫\/毛衣  包邮",
+     *                     "goodsImage": "https:\/\/img03.eelly.test\/G02\/M00\/00\/03\/small_ooYBAFqzVV2ICEGRAAER2psay8IAAABggBWRl0AARHy759.jpg",
+     *                     "goodsNumber": "2",
+     *                     "spec": "颜色:如图色,尺码:均码",
+     *                     "createdTime": "1524550066",
+     *                     "updateTime": "2018-04-24 14:07:43"
+     *                 }
+     *             ]
+     *         }
+     *     ]
+     * }
+     * )
+     *
+     */
+    public function myAppletOrders(int $tab = 0, int $page = 1, int $limit = 20, UidDTO $uidDTO = null): array;
 }
