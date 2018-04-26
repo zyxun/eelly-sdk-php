142cc9020df3118fc9a71fcc75388c9260fdadbf
runphp <runphp@qq.com>
2018-04-26 20:03:44 +0800

    add method myAppletOrders

--- src/SDK/Order/Api/SellerOrder.php
+++ src/SDK/Order/Api/SellerOrder.php
@@ -13,6 +13,19 @@ declare(strict_types=1);
 
 namespace Eelly\SDK\Order\Api;
 
-class SellerOrder
+use Eelly\DTO\UidDTO;
+use Eelly\SDK\EellyClient;
+use Eelly\SDK\Order\Service\SellerOrderInterface;
+
+
+class SellerOrder implements SellerOrderInterface
 {
+    /**
+     * @inheritDoc
+     */
+    public function myAppletOrders(int $tab = 0, int $page = 1, int $limit = 20, UidDTO $uidDTO = null): array
+    {
+        return EellyClient::request('order/sellerOrder', __FUNCTION__, true, $tab, $page, $limit);
+    }
+
 }
