a27e1c8277c67e8e8611848501fb56e8e487103c
runphp <runphp@qq.com>
2018-04-27 14:40:58 +0800

    文档整理

--- src/SDK/Order/Service/BuyerOrderInterface.php
+++ src/SDK/Order/Service/BuyerOrderInterface.php
@@ -80,6 +80,8 @@ interface BuyerOrderInterface
      *
      * @return array
      *
+     * @author hehui<hehui@eelly.net>
+     *
      * @requestExample({"tab":1, "page":2})
      *
      * @returnExample(
@@ -152,8 +154,6 @@ interface BuyerOrderInterface
      *     ]
      * }
      * )
-     *
-     * @author hehui<hehui@eelly.net>
      */
     public function listAppletOrder(int $tab = 0, int $page = 1, int $limit = 20, UidDTO $uidDTO = null): array;
 
@@ -187,47 +187,104 @@ interface BuyerOrderInterface
     public function myAppletOrderStats(UidDTO $uidDTO = null): array;
 
     /**
-     * 订单详情.
+     * 小程序订单详情(买家).
      *
+     * > 返回数据说明
+     *
+     * key | type |  value
+     * --- | ---- | -------
+     * orderStatus     | int      | 订单状态
+     * consignee       | string   | 收货人姓名
+     * mobile          | string   | 手机
+     * regionName      | string   | 省市区
+     * address         | string   | 详细地址
+     * remark          | string   | 买家留言
+     * sellerName      | string   | 卖家名称
+     * orderAmount     | int      | 实收
+     * initGoodsAmount | int      | 货款
+     * freight         | int      | 运费
+     * discountAmount  | int      | 优惠金额
+     * changePrice     | int      | 改价
+     * orderSn         | string   | 订单号
+     * createdDatetime | Datetime | 下单日期时间
+     * payDatetime     | Datetime | 支付日期时间
+     * shipDatetime    | Datetime | 发货日期时间
+     * orderfrom       | string   | 订单来源
+     * goodsCount      | int       | 商品款数
+     * productCount    | int       | 商品总件数
+     * goodsList       | array     | 商品列表
+     * goodsList[]['goodsName']    | string | 商品名称
+     * goodsList[]['price']        | int    | 商品价格
+     * goodsList[]['quantity']     | int    | 商品数量
+     * goodsList[]['spec']         | string | 商品属性
+     * goodsList[]['goodsImage']   | string | 商品图片
+     *
+     * > 订单状态(orderStatus)
+     *
+     * 值      |状态说明
+     * -------|----------
+     * 0      | 未知（错误值）
+     * 1      | 待付款
+     * 2      | 待分享
+     * 3      | 待发货
+     * 4      | 待收货
+     * 5      | 待评价
+     * 6      | 已评价
+     * 7      | 集赞失败,已退款
+     * 8      | 已退款, 交易取消
+     * 9      | 未付款, 交易取消
+     * 
      * @param int         $orderId 订单id
      * @param UidDTO|null $uidDTO  uid dto
      *
      * @return array
      *
+     * @requestExample({"orderId":"160"})
+     *
      * @returnExample(
      * {
-     *     "order_id": "160",
-     *     "order_sn": "1811370443",
-     *     "seller_name": "莫琼小店",
-     *     "os_id": "26",
-     *     "order_amount": "2",
+     *     "orderId": "160",
+     *     "orderSn": "1811370443",
+     *     "sellerName": "莫琼小店",
+     *     "osId": "26",
+     *     "orderAmount": "2",
+     *     "payTime": "1524556066",
+     *     "shipTime": "0",
      *     "freight": "1",
-     *     "created_time": 1524555994,
+     *     "createdTime": 1524555994,
      *     "remark": "",
+     *     "fromFlag": "0",
      *     "likes": "2",
      *     "evaluation": null,
-     *     "discount_amount": "0",
-     *     "init_amount": "0",
+     *     "initGoodsAmount": "2",
+     *     "discountAmount": "0",
+     *     "initAmount": "0",
      *     "consignee": "蓝厨卫",
      *     "mobile": "11113131313",
-     *     "region_name": "山西省 晋城市 沁水县",
+     *     "regionName": "山西省 晋城市 沁水县",
      *     "address": "2222",
      *     "orderStatus": 8,
      *     "createdDatetime": "2018-04-24 07:46:34",
+     *     "payDatetime": "2018-04-24 07:47:46",
+     *     "shipDatetime": "1970-01-01 00:00:00",
+     *     "changePrice": 1,
+     *     "orderfrom": "未知",
+     *     "goodsCount": 1,
+     *     "productCount": 0,
      *     "goodsList": [
      *         {
-     *             "og_id": "20000215",
-     *             "order_id": "160",
-     *             "goods_id": "1450168293",
-     *             "gs_id": "195022196",
+     *             "ogId": "20000215",
+     *             "orderId": "160",
+     *             "goodsId": "1450168293",
+     *             "gsId": "195022196",
      *             "price": "1",
      *             "quantity": "2",
-     *             "goods_name": "【莫琼小店】 2018新款 针织衫\/毛衣  包邮",
-     *             "goods_image": "https:\/\/img03.eelly.test\/G02\/M00\/00\/03\/small_ooYBAFqzVV2ICEGRAAER2psay8IAAABggBWRl0AARHy759.jpg",
-     *             "goods_number": "2",
+     *             "goodsName": "【莫琼小店】 2018新款 针织衫\/毛衣  包邮",
+     *             "goodsImage": "https:\/\/img03.eelly.test\/G02\/M00\/00\/03\/small_ooYBAFqzVV2ICEGRAAER2psay8IAAABggBWRl0AARHy759.jpg",
+     *             "goodsNumber": "2",
      *             "spec": "颜色:如图色,尺码:均码",
-     *             "created_time": "1524555994",
-     *             "update_time": "2018-04-24 15:46:32"
+     *             "createdTime": "1524555994",
+     *             "updateTime": "2018-04-24 15:46:32"
      *         }
      *     ]
      * }
--- src/SDK/Order/Service/SellerOrderInterface.php
+++ src/SDK/Order/Service/SellerOrderInterface.php
@@ -208,6 +208,21 @@ interface SellerOrderInterface
      * goodsList[]['spec']         | string | 商品属性
      * goodsList[]['goodsImage']   | string | 商品图片
      *
+     * > 订单状态(orderStatus)
+     *
+     * 值      |状态说明
+     * -------|----------
+     * 0      | 未知（错误值）
+     * 1      | 待付款
+     * 2      | 待分享
+     * 3      | 待发货
+     * 4      | 待收货
+     * 5      | 待评价
+     * 6      | 已评价
+     * 7      | 集赞失败,已退款
+     * 8      | 已退款, 交易取消
+     * 9      | 未付款, 交易取消
+     *
      * @param int         $orderId 订单id
      * @param UidDTO|null $uidDTO  uid dto
      *
