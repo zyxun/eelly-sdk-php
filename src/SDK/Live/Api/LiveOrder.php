82ec8bfb8418f8d02b1dee6ed2829d0b3b2ccbf9
runphp <runphp@qq.com>
2018-05-11 15:30:28 +0800

    add use class

--- src/SDK/Live/Api/LiveOrder.php
+++ src/SDK/Live/Api/LiveOrder.php
@@ -10,6 +10,7 @@ namespace Eelly\SDK\Live\Api;
 
 
 use Eelly\DTO\UidDTO;
+use Eelly\SDK\EellyClient;
 use Eelly\SDK\Live\Service\LiveOrderInterface;
 
 class LiveOrder implements LiveOrderInterface
