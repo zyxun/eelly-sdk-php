5f619676361f44a83656bf6d3e53394dd780772c
wechan <a421083@qq.com>
2018-05-23 17:35:49 +0800

    拼货二期 - 资金管理 - 收支明细

--- src/SDK/Pay/Api/AppletAccount.php
+++ src/SDK/Pay/Api/AppletAccount.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 /*
  * This file is part of eelly package.
  *
@@ -13,10 +12,14 @@ declare(strict_types=1);
 
 namespace Eelly\SDK\Pay\Api;
 
-use Eelly\DTO\UidDTO;
 use Eelly\SDK\EellyClient;
 use Eelly\SDK\Pay\Service\AppletAccountInterface;
+use Eelly\DTO\UidDTO;
 
+/**
+ *
+ * @author shadonTools<localhost.shell@gmail.com>
+ */
 class AppletAccount implements AppletAccountInterface
 {
     /**
@@ -41,7 +44,32 @@ class AppletAccount implements AppletAccountInterface
      */
     public function statistics(int $storeId): array
     {
-        return EellyClient::request('pay/appletAccount', __FUNCTION__, true);
+        return EellyClient::request('pay/appletAccount', 'statistics', true, $storeId);
+    }
+
+    /**
+     * 小程序资金统计数据.
+     *
+     * > 返回数据说明
+     *
+     * key | type |  value
+     * --- | ---- | -------
+     * remainingMoney          | int    | 账户余额(分)
+     * todayGainingsMoney      | int    | 今日收入(分)
+     * gainingsMoney           | int    | 累计收入(分)
+     * waitingSettlementMoney  | int    | 等待结算(分)
+     * payingMoney             | int    | 累计支出(分)
+     *
+     * @param int $storeId 店铺id
+     *
+     * @return array
+     *
+     * @author hehui<hehui@eelly.net>
+     * @author wechan
+     */
+    public function statisticsAsync(int $storeId)
+    {
+        return EellyClient::request('pay/appletAccount', 'statistics', false, $storeId);
     }
 
     /**
@@ -101,6 +129,79 @@ class AppletAccount implements AppletAccountInterface
      */
     public function myBindBanks(UidDTO $uidDTO = null): array
     {
-        return EellyClient::request('pay/appletAccount', __FUNCTION__, true);
+        return EellyClient::request('pay/appletAccount', 'myBindBanks', true, $uidDTO);
+    }
+
+    /**
+     * 获取我绑定的银行账户.
+     *
+     * > 返回数据说明
+     *
+     * key | type |  value
+     * --- | ---- | -------
+     * []pbId              | int    |   用户银行id
+     * []bankName          | int    |   银行名称
+     * []bankAccount       | string |   银行账号
+     * []realName          | strng  |   真实姓名
+     * []isDefault         | bool   |   是否默认卡
+     * []bankLogo          | string |   银行logo
+     *
+     *
+     * @param UidDTO|null $uidDTO uid dto
+     *
+     * @return array
+     *
+     * @returnExample(
+     * [
+     *     {
+     *         "pbId": "6",
+     *         "userId": "148086",
+     *         "gbCode": "0",
+     *         "bankId": "1",
+     *         "bankName": "中行上海分行",
+     *         "bankSubbranch": "",
+     *         "bankAccount": "9843010902492123",
+     *         "realName": "molimoq",
+     *         "phone": "13800138000",
+     *         "isDefault": "0",
+     *         "createdTime": "1510388565",
+     *         "updateTime": "2017-11-11 16:22:45",
+     *         "bankLogo": "https:\/\/img.eelly.test\/G01\/M00\/00\/06\/oYYBAFsAKTaIDt6KAAaGplFOcjsAAACagCL3MEABoa-952.jpg"
+     *     },
+     *     {
+     *         "pbId": "7",
+     *         "userId": "148086",
+     *         "gbCode": "0",
+     *         "bankId": "2",
+     *         "bankName": "中行上海分行",
+     *         "bankSubbranch": "",
+     *         "bankAccount": "9843010902492123",
+     *         "realName": "molimoq",
+     *         "phone": "13800138000",
+     *         "isDefault": "0",
+     *         "createdTime": "1510388565",
+     *         "updateTime": "2018-05-20 10:33:44",
+     *         "bankLogo": "https:\/\/img.eelly.test\/G02\/M00\/00\/03\/ooYBAFsAKUCIKRtNAACxzNPHCRoAAABgwCtsosAALHk575.png"
+     *     }
+     * ])
+     *
+     * @author hehui<hehui@eelly.net>
+     */
+    public function myBindBanksAsync(UidDTO $uidDTO = null)
+    {
+        return EellyClient::request('pay/appletAccount', 'myBindBanks', false, $uidDTO);
+    }
+
+    /**
+     * @return self
+     */
+    public static function getInstance(): self
+    {
+        static $instance;
+        if (null === $instance) {
+            $instance = new self();
+        }
+
+        return $instance;
     }
 }
