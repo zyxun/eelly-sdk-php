8f43dae01037d563f0ec39005d658a30b6efc1a2
sunanzhi <sunanzhi@hotmail.com>
2018-05-25 18:50:07 +0800

    添加注释

--- src/SDK/Live/Service/LiveShareInterface.php
+++ src/SDK/Live/Service/LiveShareInterface.php
@@ -25,39 +25,47 @@ interface LiveShareInterface
     /**
      * 直播间分享.
      *
-     * @param int    $live_id 当前进入的直播间id
-     * @param string $type    直播间的类型 [app-分享、pc端-分享、小程序-分享]
-     * @param UidDTO $user    当前登陆的用户
-     *
-     * @requestExample ({
-     *    "live_id":1,
-     *    "type":"小程序-分享"
+     * @param int    $liveId 当前进入的直播间id
+     * @param string $type   直播间的类型 [app-分享、pc端-分享、小程序-分享]
+     * @param UidDTO $user   当前登陆的用户
+     * @requestExample({
+     *     "liveId":1,
+     *     "type":"小程序-分享"
      * })
-     * @returnExample ({
-     *    unique_flag:'5b07c5b5b7490',
+     * @returnExample({
+     *     "code":200,
+     *     "msg":"分享信息记录成功",
+     *     "data":{
+     *      "uniqueFlag":"5b07c5b5b7490"
+     *    }
      * })
      *
      * @author sunanzhi<sunanzhi@hotmail.com>
      *
      * @since 2018年5月25日
      */
-    public function share(int $live_id, string $type = null, UidDTO $user = null);
+    public function share(int $liveId, string $type = null, UidDTO $user = null);
 
     /**
      * 进入分享反馈.
      *
-     * @param string $unique_flag 直播间分享返回的唯一标识
-     * @param string $type        进入直播间的类型 [小程序进入、pc端进入]
-     * @param UidDTO $user        当前登陆的用户
+     * @param string $uniqueFlag 直播间分享返回的唯一标识
+     * @param string $type       进入直播间的类型 [小程序进入、pc端进入]
+     * @param UidDTO $user       当前登陆的用户
      *
-     * @requestExample ({
-     *    "unique_flag":'5b07c5b5b7490',
-     *    "type":"小程序进入"
+     * @requestExample({
+     *     "uniqueFlag":"5b07c5b5b7490",
+     *     "type":"小程序进入"
+     * })
+     *
+     * @returnExample({
+     *     "code":200,
+     *     "msg":"反馈记录成功"
      * })
      *
      * @author sunanzhi<sunanzhi@hotmail.com>
      *
      * @since 2018年5月25日
      */
-    public function shareFeedback(string $unique_flag, string $type = null, UidDTO $user = null);
+    public function shareFeedback(string $uniqueFlag, string $type = null, UidDTO $user = null);
 }
