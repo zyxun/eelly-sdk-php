e2e0b07e297cec2ea3d8f36144ddee49ea0d24cc
sunanzhi <sunanzhi@hotmail.com>
2018-05-28 20:17:46 +0800

    更改反馈参数，不需要user

--- src/SDK/Live/Service/LiveShareInterface.php
+++ src/SDK/Live/Service/LiveShareInterface.php
@@ -25,9 +25,10 @@ interface LiveShareInterface
     /**
      * 直播间分享.
      *
-     * @param int    $liveId 当前进入的直播间id
-     * @param string $type   直播间的类型 [app-分享、pc端-分享、小程序-分享]
-     * @param UidDTO $user   当前登陆的用户
+     * @param int    $liveId     当前进入的直播间id
+     * @param string $uniqueFlag 分享的唯一标识 
+     * @param string $type       直播间的类型 [app-分享、pc端-分享、小程序-分享]
+     * @param UidDTO $user       当前登陆的用户 
      * 
      * @return string
      * 
@@ -52,7 +53,6 @@ interface LiveShareInterface
      *
      * @param string $uniqueFlag 直播间分享提供的唯一标识
      * @param string $type       进入直播间的类型 [小程序进入、pc端进入]
-     * @param UidDTO $user       当前登陆的用户
      *
      * @return string
      * 
@@ -70,5 +70,5 @@ interface LiveShareInterface
      *
      * @since 2018年5月25日
      */
-    public function shareFeedback(string $uniqueFlag, string $type = null, UidDTO $user = null):string;
+    public function shareFeedback(string $uniqueFlag, string $type = null):string;
 }
