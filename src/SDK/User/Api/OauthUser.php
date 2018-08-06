7e698d567748e964387b0a570db252a3c1cce50b
hehui <runphp@qq.com>
2018-08-06 17:13:06 +0800

    迁移部分接口

---
+++ src/SDK/User/Api/OauthUser.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\User\Api;
+
+use Eelly\DTO\UserDTO;
+use Eelly\SDK\EellyClient;
+use Eelly\SDK\User\Service\OauthUserInterface;
+
+class OauthUser implements OauthUserInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getUserByPassword(string $username, string $password): UserDTO
+    {
+        return EellyClient::request('user/user', __FUNCTION__, true, $username, $password);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUser(int $uid): UserDTO
+    {
+        return EellyClient::request('user/user', __FUNCTION__, true, $uid);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUserByQQAccessToken(string $accessToken): UserDTO
+    {
+        return EellyClient::request('user/user', __FUNCTION__, true, $code);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUserByWechatCode(string $code): UserDTO
+    {
+        return EellyClient::request('user/user', __FUNCTION__, true, $code);
+    }
+}
--- src/SDK/User/Service/UserInterface.php
+++ src/SDK/User/Service/UserInterface.php
@@ -164,6 +164,8 @@ interface UserInterface
      * @returnExample({"uid":148086,"username":"molimoq","mobile":"13800138000"})
      *
      * @author hehui<hehui@eelly.net>
+     *
+     * @deprecated
      */
     public function getUserByPassword(string $username, string $password): UserDTO;
 
@@ -440,13 +442,14 @@ interface UserInterface
      * @author hehui<hehui@eelly.net>
      *
      * @since 2017-11-06
+     * @deprecated
      */
     public function getUser(int $uid): UserDTO;
 
     /**
      * 通过QQ token获取用户信息.
      *
-     * * ### 返回数据说明
+     * ### 返回数据说明
      *
      * 字段|类型|说明
      * ---------|-------|--------------
@@ -461,6 +464,8 @@ interface UserInterface
      * @returnExample({"uid":148086,"username":"molimoq","mobile":"13800138000"})
      *
      * @author hehui<hehui@eelly.net>
+     *
+     * @deprecated
      */
     public function getUserByQQAccessToken(string $accessToken): UserDTO;
 }
