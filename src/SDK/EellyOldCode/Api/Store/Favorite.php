a9a49b03e73e62d52667eefe4d71b7a8784f62ce
hehui <runphp@qq.com>
2018-08-07 18:33:52 +0800

    添加旧代码sdk

---
+++ src/SDK/EellyOldCode/Api/Store/Favorite.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\EellyOldCode\Api\Store;
+
+/**
+ * Class Favorite
+ * var/eelly-old-code/modules/Store/Service/FavoriteService.php.
+ */
+class Favorite
+{
+    /**
+     * @param $userId
+     * @param array $storeIds
+     *
+     * @return mixed
+     */
+    public function isCollectByUserId($userId, array $storeIds)
+    {
+        return \Eelly\SDK\EellyClient::request('eellyOldCode/store/favorite', __FUNCTION__, true, $uid, $storeId);
+    }
+}
