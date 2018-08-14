ae13207b6b9e4a51db7ad0580814261866e003ba
wechan <a421083@qq.com>
2018-08-14 09:53:45 +0800

    订单中心 - 下单列表

---
+++ src/SDK/EellyOldCode/Api/Cart/Cart.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\EellyOldCode\Api\Cart;
+
+use Eelly\SDK\EellyClient;
+
+/**
+ * Class Cart.
+ *
+ * var/eelly-old-code/modules/Cart/Service/CartService.php
+ *
+ * @author wechan
+ */
+class Cart
+{
+    /**
+     * @param array $goodsIds 商品id
+     *
+     * @return mixed
+     */
+    public function getGoodsInfo(array $goodsIds)
+    {
+        return EellyClient::request('eellyOldCode/cart/getGoodsInfo', __FUNCTION__, true, $goodsIds);
+    }
+}
