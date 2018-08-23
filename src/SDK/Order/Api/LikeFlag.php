717e4a51a1ef58d2591d4cee2fee0e30ea48d2cf
wechan <a421083@qq.com>
2018-08-23 22:16:56 +0800

    1元商品 下单逻辑更改

---
+++ src/SDK/Order/Api/LikeFlag.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\Order\Api;
+
+use Eelly\SDK\EellyClient;
+use Eelly\SDK\Order\Service\LikeFlagInterface;
+
+/**
+ *
+ * @author shadonTools<localhost.shell@gmail.com>
+ */
+class LikeFlag implements LikeFlagInterface
+{
+    /**
+     * 根据商品id 获取指定一元商品下单的数量
+     * 
+     * @param array $goodsIds 商品id
+     * 
+     * @author wechan
+     * @since 2018年08月23日
+     */
+    public function countLikeFlagOrder(array $goodsIds): array
+    {
+        return EellyClient::request('order/likeFlag', 'countLikeFlagOrder', true, $goodsIds);
+    }
+
+    /**
+     * 根据商品id 获取指定一元商品下单的数量
+     * 
+     * @param array $goodsIds 商品id
+     * 
+     * @author wechan
+     * @since 2018年08月23日
+     */
+    public function countLikeFlagOrderAsync(array $goodsIds)
+    {
+        return EellyClient::request('order/likeFlag', 'countLikeFlagOrder', false, $goodsIds);
+    }
+
+    /**
+     * @return self
+     */
+    public static function getInstance(): self
+    {
+        static $instance;
+        if (null === $instance) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+}
