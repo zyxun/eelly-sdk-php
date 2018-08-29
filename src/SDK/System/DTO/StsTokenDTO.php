b094970bb236e3a2b83b19441160b5976ed9c0d9
hehui <runphp@qq.com>
2018-08-29 18:11:36 +0800

    新增获取OSS的STS凭证.

---
+++ src/SDK/System/DTO/StsTokenDTO.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\System\DTO;
+
+use Eelly\DTO\AbstractDTO;
+
+/**
+ * Sts token DTO.
+ *
+ * @author hehui<hehui@eelly.net>
+ */
+class StsTokenDTO extends AbstractDTO
+{
+    /**
+     * AccessKeyId.
+     *
+     * @var string
+     */
+    public $accessKeySecret;
+
+    /**
+     * AccessKeySecret.
+     *
+     * @var string
+     */
+    public $accessKeyId;
+
+    /**
+     * Token.
+     *
+     * @var string
+     */
+    public $expiration;
+
+    /**
+     * 该Token失效的时间.
+     *
+     * @var string
+     */
+    public $securityToken;
+}
