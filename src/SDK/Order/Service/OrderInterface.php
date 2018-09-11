e1d02dda5d73791709b0697b3645a8af05b40bd3
wechan <a421083@qq.com>
2018-09-11 20:37:16 +0800

    订单中心 - 下单列表

--- src/SDK/Order/Service/OrderInterface.php
+++ src/SDK/Order/Service/OrderInterface.php
@@ -734,7 +734,7 @@ interface OrderInterface
      * @param int $data['goods'][0]['isSpelling'] 是否拼团商品
      * @param array $data['uniqueIds'][] 购物车商品主键id (购物车下单必传,立即下单传空)
      * 
-     * @returnExample({"defaultAddress":[],"storeOrderGoods":[{"storeId":"16777306","storeName":"新店6","goodsInfo":[{"goodsId":5578748,"totalPrice":1860,"goodsNumber":155,"goodsSn":"港湾123","goodsImage":"https:\/\/img02.eelly.test\/G01\/M00\/00\/06\/small_oYYBAFmJIJCIVphJAAEbJKKDQNIAAACZALBulgAARs8034.jpg","goodsName":"十三行女装流行风格","specInfo":[{"specId":"32090625","price":"12.00","originalPrice":"12.00","quantity":107,"color":"黑色","size":"XXS","stock":99998},{"specId":"32090626","price":"12.00","originalPrice":"12.00","quantity":48,"color":"绿色","size":"XXS","stock":100000}],"priceData":{"goodsId":"5578748","storeId":"16777306","priceType":1,"priceLower":"12.00","priceUpper":"12.00","priceData":[{"lowerLimit":"1","upperLimit":"0","price":"12.00","type":"1"}]}}],"couponInfo":[{"couponId":1450168327,"couponNo":"1450168327SS","startTime":1525329993,"endTime":1525329993,"recId":111}],"expressWay":[{"name":"货运","shippingId":"222789","expressType":0,"express_select":"1","freight":0,"weight":0},{"name":"运费到付","shippingId":"222789","expressType":"1","expressSelect":"1","freight":0,"weight":0}],"defaultAddress":{"addrId":"547435","userName":"hahhahah","telNumber":"13232343244","detailInfo":"北京市 市辖区 东城区 sdhfjkdfhkdjsfhdsfdf","default":"1","regionId":"110101"}}],"freePostCard":[]})
+     * @returnExample({"defaultAddress":{"addrId":"547435","userName":"hahhahah","telNumber":"13232343244","detailInfo":"北京市 市辖区 东城区 sdhfjkdfhkdjsfhdsfdf","default":"1","regionId":"110101"},"storeOrderGoods":[{"storeId":"16777306","storeName":"新店6","goodsInfo":[{"goodsId":5578748,"totalPrice":1860,"goodsNumber":155,"goodsSn":"港湾123","goodsImage":"https:\/\/img02.eelly.test\/G01\/M00\/00\/06\/small_oYYBAFmJIJCIVphJAAEbJKKDQNIAAACZALBulgAARs8034.jpg","goodsName":"十三行女装流行风格","specInfo":[{"specId":"32090625","price":"12.00","originalPrice":"12.00","quantity":107,"color":"黑色","size":"XXS","stock":99998}],"priceData":{"goodsId":"5578748","storeId":"16777306","priceType":1,"priceLower":"12.00","priceUpper":"12.00","priceData":[{"lowerLimit":"1","upperLimit":"0","price":"12.00","type":"1"}]}}],"couponInfo":[{"couponId":1450168327,"couponNo":"1450168327SS","startTime":1525329993,"endTime":1525329993,"recId":111}],"expressWay":[{"name":"货运","shippingId":"222789","expressType":0,"express_select":"1","freight":0,"weight":0},{"name":"运费到付","shippingId":"222789","expressType":"1","expressSelect":"1","freight":0,"weight":0}]}],"freePostCard":["暂时给个默认值"]})
      * 
      * @param UidDTO $user
      * 
@@ -827,4 +827,21 @@ interface OrderInterface
      *
      */
     public function getOrderCouponList(float $money, int $storeId, int $userId):array;
+    
+    /**
+     * 订单发起的支付.
+     *
+     * @param array  $orderIds 多个订单Id
+     * @param string $type  支付账号类型 wechat:微信支付 smallWechat:微信小程序 alipay:支付宝
+     * @param array $extend 扩展信息,比如某宝账号,某小程序账号信息。
+     * @param UidDTO $user      登录用户信息
+     * 
+     * @return array
+     * 
+     * @returnExample({{"platform":"alipayApp","billNo":"201809110166529825","data":{"alipay_sdk":"lokielse\/omnipay-alipay","app_id":"2016122204515132","biz_content":"{\"out_trade_no\":\"201809110166529825\",\"total_amount\":0.04,\"subject\":\"\\u8ba2\\u5355\\u652f\\u4ed8\",\"body\":\"\",\"goods_type\":1,\"passback_params\":\"paId%3D12634%26userId%3D2108412%26account%3D126mail.wap%26type%3D1%26transact%3D2%26platform%3DalipayApp\",\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}","charset":"UTF-8","format":"JSON","method":"alipay.trade.app.pay","notify_url":"https:\/\/cs.blty.com\/alipay\/notifyV2.html","sign_type":"RSA","timestamp":"2018-09-11 18:28:49","version":"1.0","sign":"Mf3VsqP7YZK9nGJUgkNFF8Rc7VgD\/aXSzYIZlBM7aFye3lCQVvrHAi6trUzwMPTfWGKULHWRMRrOPhSw2pEqgsFCeIVAe7GPP8zghleBwTDc6MKnB45yDbIcLmM2H9C7u8Xk7Kru93cTGwW3PJL6GgEJs8\/hYN14MnadBaBxiXI=","order_string":"alipay_sdk=lokielse%2Fomnipay-alipay&app_id=2016122204515132&biz_content=%7B%22out_trade_no%22%3A%22201809110166529825%22%2C%22total_amount%22%3A0.04%2C%22subject%22%3A%22%5Cu8ba2%5Cu5355%5Cu652f%5Cu4ed8%22%2C%22body%22%3A%22%22%2C%22goods_type%22%3A1%2C%22passback_params%22%3A%22paId%253D12634%2526userId%253D2108412%2526account%253D126mail.wap%2526type%253D1%2526transact%253D2%2526platform%253DalipayApp%22%2C%22product_code%22%3A%22FAST_INSTANT_TRADE_PAY%22%7D&charset=UTF-8&format=JSON&method=alipay.trade.app.pay&notify_url=https%3A%2F%2Fcs.blty.com%2Falipay%2FnotifyV2.html&sign_type=RSA&timestamp=2018-09-11+18%3A28%3A49&version=1.0&sign=Mf3VsqP7YZK9nGJUgkNFF8Rc7VgD%2FaXSzYIZlBM7aFye3lCQVvrHAi6trUzwMPTfWGKULHWRMRrOPhSw2pEqgsFCeIVAe7GPP8zghleBwTDc6MKnB45yDbIcLmM2H9C7u8Xk7Kru93cTGwW3PJL6GgEJs8%2FhYN14MnadBaBxiXI%3D"},"orderSns":["2153610960734762"],"orderIds":[50001771]}})
+     *
+     * @author wechan
+     * @since 2018年09月10日
+     */
+    public function orderPay(array $orderIds, string $type = 'wechat', $extend = [], UidDTO $user = null): array;
 }
