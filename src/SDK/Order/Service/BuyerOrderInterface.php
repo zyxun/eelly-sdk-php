b3b23dfb6cee0ac65477f0a32b5679d18dd0f5ca
runphp <runphp@qq.com>
2018-09-12 17:46:07 +0800

    新增接口

--- src/SDK/Order/Service/BuyerOrderInterface.php
+++ src/SDK/Order/Service/BuyerOrderInterface.php
@@ -23,20 +23,19 @@ use Eelly\DTO\UidDTO;
 interface BuyerOrderInterface
 {
     /**
-     * 我的买家订单分页.
+     * 订单分页(买家).
      *
-     * @param int $tab  订单状态筛选值(0: 全部订单 1: 代付款 2: 待分享 3: 待发货 4: 待收货 5: 待评价 6: 退货退款中)
-     * @param int $page 第几页
-     * @param int $limit  分页大小
-     * @param array|string $searchParams  搜索参数(类型为array时进行精确搜索)
-     * @param string $searchParams['ordersn']  订单号
-     * @param string $searchParams['storename']  店铺名或卖家用户名
-     * @param string $searchParams['consignee']    收货人
-     * @param string $searchParams['goodsname']  商品名或货号
-     * @param int $searchParams['ordertimeStart']  下单开始时间戳
-     * @param int $searchParams['ordertimeEnd'] 下单结束时间戳
-     *
-     * @param UidDTO|null $uidDTO  uid dto
+     * @param int          $tab                            订单状态筛选值(0: 全部订单 1: 代付款 2: 待分享 3: 待发货 4: 待收货 5: 待评价 6: 退货退款中)
+     * @param int          $page                           第几页
+     * @param int          $limit                          分页大小
+     * @param array|string $searchParams                   搜索参数(类型为array时进行精确搜索)
+     * @param string       $searchParams['ordersn']        订单号
+     * @param string       $searchParams['storename']      店铺名或卖家用户名
+     * @param string       $searchParams['consignee']      收货人
+     * @param string       $searchParams['goodsname']      商品名或货号
+     * @param int          $searchParams['ordertimeStart'] 下单开始时间戳
+     * @param int          $searchParams['ordertimeEnd']   下单结束时间戳
+     * @param UidDTO|null  $uidDTO                         uid dto
      *
      * @return array
      *
@@ -111,8 +110,8 @@ interface BuyerOrderInterface
      *
      * @author hehui<hehui@eelly.net>
      */
-    public function myOrderPage(int $tab = 0, int $page = 1, int $limit = 20, $searchParams = null, UidDTO $uidDTO = null):array;
-    
+    public function orderPage(int $tab = 0, int $page = 1, int $limit = 20, $searchParams = null, UidDTO $uidDTO = null): array;
+
     /**
      * 订单详情(买家).
      *
@@ -243,40 +242,65 @@ interface BuyerOrderInterface
      *
      * @author hehui<hehui@eelly.net>
      */
-    public function myOrderDetail(int $orderId, UidDTO $uidDTO = null): array;
+    public function orderDetails(int $orderId, UidDTO $uidDTO = null): array;
 
     /**
-     * 确认收货.
+     * 通知商家发送我的某个订单商品.
      *
-     * @param int $orderId 订单id
-     * @param int $uid 用户id
+     * @param int         $orderId 订单id
+     * @param UidDTO|null $uidDTO  uid dto
      *
      * @return bool
      *
      * @author hehui<hehui@eelly.net>
      */
-    public function confirmReceivedOrder(int $orderId, int $uid):bool;
+    public function notifySendProducts(int $orderId, UidDTO $uidDTO = null): bool;
 
     /**
-     * 申请退货退款
+     * 取消某个订单.
      *
-     * @param integer $type 退货退款类型 1:仅退款， 2:退货退款
-     * @param integer $orderId 订单id
-     * @param integer $remarkType 退款原因
-     * @param integer $price 退款金额
-     * @param string $desc 退款说明
+     * @param int         $orderId 订单id
+     * @param UidDTO|null $uidDTO  uid dto
+     *
+     * @return bool
+     *
+     * @author hehui<hehui@eelly.net>
+     */
+    public function cancelOrder(int $orderId, UidDTO $uidDTO = null): bool;
+
+    /**
+     * 确认收货.
+     *
+     * @param int $orderId 订单id
+     * @param int $uid     用户id
+     *
+     * @return bool
+     *
+     * @author hehui<hehui@eelly.net>
+     */
+    public function confirmReceivedOrder(int $orderId, UidDTO $uidDTO = null): bool;
+
+    /**
+     * 申请退货退款.
+     *
+     * @param int    $type        退货退款类型 1:仅退款， 2:退货退款
+     * @param int    $orderId     订单id
+     * @param int    $remarkType  退款原因
+     * @param int    $price       退款金额
+     * @param string $desc        退款说明
      * @param string $certificate 退款凭证 图片，多图用#拼接
-     * @return boolean
-     * 
+     *
+     * @return bool
+     *
      * @author sunanzhi <sunanzhi@hotmail.com>
      */
-    public function orderRefund(int $type, int $orderId, int $remarkType, int $price, string $desc = '-', string $certificate = '-'):bool;
+    public function orderRefund(int $type, int $orderId, int $remarkType, int $price, string $desc = '-', string $certificate = '-'): bool;
 
     /**
-     * 退货退款详情
-     * 
-     * > from_os_id 和 to_os_id 状态说明 
-     * 
+     * 退货退款详情.
+     *
+     * > from_os_id 和 to_os_id 状态说明
+     *
      *  key    |  value
      *  ------ | -------
      *  16     | 申请退货退款
@@ -288,14 +312,14 @@ interface BuyerOrderInterface
      *  25     | 退货退款成功
      *  26     | 交易取消
      *  27     | 退款取消
-     * 
+     *
      * > remark_type 退货原因状态
-     * 
+     *
      *  key    |  value
      *  ------ | -------
      *  0      | 其他
-     *  3      | 商品质量不好 
-     *  4      | 卖家超时未发货 
+     *  3      | 商品质量不好
+     *  4      | 卖家超时未发货
      *  5      | 卖家发错货
      *  7      | 商品与描述不符
      *  8      | 收到商品破损
@@ -308,8 +332,8 @@ interface BuyerOrderInterface
      * key | type |  value
      * --- | ---- | -------
      * applyAmount      | int      | 申请的金额
-     * applyFreight     | int      | 申请的运费 
-     * type             | int      | 退货退款类型: 1 退款 2 退货 
+     * applyFreight     | int      | 申请的运费
+     * type             | int      | 退货退款类型: 1 退款 2 退货
      * remarkType       | string   | 退货退款原因
      * remark           | string   | 备注说明
      * certificate      | int      | 凭证
@@ -322,10 +346,11 @@ interface BuyerOrderInterface
      * time             | int      | 倒计时的时间
      * statusContent    | string   | 详情文案信息
      * status           | int      | 文案状态 1:申请退款中,2:申请退货中,3:卖家拒绝退款，等待我处理,4:卖家同意退货，等待我退货,5:卖家拒绝退货，等待我处理,6:我已发货，等待卖家发货,7:退货退款成功
-     * 
-     * @param integer $orderId 订单id
+     *
+     * @param int $orderId 订单id
+     *
      * @return array
-     * 
+     *
      * @requestExample({"orderId":116})
      * @returnExample({
      *      "applyAmount":"10",
@@ -344,16 +369,16 @@ interface BuyerOrderInterface
      *      "lastTime":"1529913120",
      *      "time":"9281"
      * })
-     * 
+     *
      * @author sunanzhi <sunanzhi@hotmail.com>
      */
-    public function orderDetail(int $orderId):array;
+    public function orderDetail(int $orderId): array;
 
     /**
-     * 协商记录
-     * 
-     * > from_os_id 和 to_os_id 状态说明 
-     * 
+     * 协商记录.
+     *
+     * > from_os_id 和 to_os_id 状态说明
+     *
      *  key    |  value
      *  ------ | -------
      *  16     | 申请退货退款
@@ -365,27 +390,27 @@ interface BuyerOrderInterface
      *  25     | 退货退款成功
      *  26     | 交易取消
      *  27     | 退款取消
-     * 
+     *
      * > remark_type 退货原因状态
-     * 
+     *
      *  key    |  value
      *  ------ | -------
      *  0      | 其他
-     *  3      | 商品质量不好 
-     *  4      | 卖家超时未发货 
+     *  3      | 商品质量不好
+     *  4      | 卖家超时未发货
      *  5      | 卖家发错货
      *  7      | 商品与描述不符
      *  8      | 收到商品破损
      *  11     | 商品缺货
      *  12     | 与卖家协商一致退款
      *  13     | 不想要了／拍错了
-     * 
+     *
      * > 返回数据说明
      *
      * key | type |  value
      * --- | ---- | -------
      * order_id      | int      | 订单id
-     * type          | int      | 操作类型 1:买家，2:卖家 
+     * type          | int      | 操作类型 1:买家，2:卖家
      * handle_id     | int      | 买家id
      * handle_name   | string   | 买家名称
      * from_os_id    | int      | 原先状态id
@@ -409,8 +434,9 @@ interface BuyerOrderInterface
      * address - region_name      | string   | 地区
      * address - address          | string   | 详细地址
      * address - consignee        | string   | 收件人姓名
-     * 
-     * @param integer $orderId 订单id
+     *
+     * @param int $orderId 订单id
+     *
      * @return array
      * @requestExample({"orderId":116})
      * @returnExample([
@@ -455,31 +481,32 @@ interface BuyerOrderInterface
      *      }]
      * }
      * ])
-     * 
-     * @author sunanzhi <sunanzhi@hotmail.com>
-     */
-    public function orderRefundLog(int $orderId):array;
-
-    /**
-     * 撤销退款申请
      *
-     * @param integer $orderId 订单id
-     * @return boolean
-     * 
      * @author sunanzhi <sunanzhi@hotmail.com>
      */
-    public function cancelOrderRefund(int $orderId):bool;
-
+    public function orderRefundLog(int $orderId): array;
 
     /**
-     * 退货
-     * 
-     * @param integer $orderId 订单id
+     * 撤销退款申请.
+     *
+     * @param int $orderId 订单id
+     *
+     * @return bool
+     *
+     * @author sunanzhi <sunanzhi@hotmail.com>
+     */
+    public function cancelOrderRefund(int $orderId): bool;
+
+    /**
+     * 退货.
+     *
+     * @param int    $orderId     订单id
      * @param string $invoiceInfo 收件人信息 json格式
      * @param string $invoiceCode 送货编码 快递公司拼音
      * @param string $invoiceName 快递公司
-     * @param string $invoiceNo 订单编号
-     * @return boolean
+     * @param string $invoiceNo   订单编号
+     *
+     * @return bool
      * @requestExample(
      * {
      *      "orderId":"116",
@@ -489,16 +516,16 @@ interface BuyerOrderInterface
      *      "invoiceNo":"123123"
      * }
      * )
+     *
      * @author sunanzhi <sunanzhi@hotmail.com>
      */
-    public function orderRefundInvoice(int $orderId, string $invoiceInfo, string $invoiceCode, string $invoiceName, string $invoiceNo):bool;
-
+    public function orderRefundInvoice(int $orderId, string $invoiceInfo, string $invoiceCode, string $invoiceName, string $invoiceNo): bool;
 
     /**
-     * 重新申请退货退款接口
-     * 
-     * > from_os_id 和 to_os_id 状态说明 
-     * 
+     * 重新申请退货退款接口.
+     *
+     * > from_os_id 和 to_os_id 状态说明
+     *
      *  key    |  value
      *  ------ | -------
      *  16     | 申请退货退款
@@ -510,21 +537,21 @@ interface BuyerOrderInterface
      *  25     | 退货退款成功
      *  26     | 交易取消
      *  27     | 退款取消
-     * 
+     *
      * > remark_type 退货原因状态
-     * 
+     *
      *  key    |  value
      *  ------ | -------
      *  0      | 其他
-     *  3      | 商品质量不好 
-     *  4      | 卖家超时未发货 
+     *  3      | 商品质量不好
+     *  4      | 卖家超时未发货
      *  5      | 卖家发错货
      *  7      | 商品与描述不符
      *  8      | 收到商品破损
      *  11     | 商品缺货
      *  12     | 与卖家协商一致退款
      *  13     | 不想要了／拍错了
-     * 
+     *
      * > 返回数据说明
      *
      * key | type |  value
@@ -538,8 +565,9 @@ interface BuyerOrderInterface
      * phase         | int       | 订单状态 1:未发货发起的退款，2:已发货发起的退款 3:已发货发起的退货退款
      * os_id         | int       | 订单状态
      * certificate   | array       | 凭证数据 只有type为2时才有有效数据
-     * 
-     * @param integer $orderId 订单id
+     *
+     * @param int $orderId 订单id
+     *
      * @return array
      * @requestExample({"orderId":"116"})
      * @returnExample({
@@ -553,29 +581,29 @@ interface BuyerOrderInterface
      *      "os_id":"26",
      *      "certificate":"[]"
      * })
-     * 
+     *
      * @author sunanzhi <sunanzhi@hotmail.com>
      */
-    public function orderRefundLogEdit(int $orderId):array;
-
+    public function orderRefundLogEdit(int $orderId): array;
 
     /**
-     * 修改退货信息
+     * 修改退货信息.
      *
-     * @param integer $oiId 退货id
-     * @param integer $orderId 订单id
+     * @param int    $oiId        退货id
+     * @param int    $orderId     订单id
      * @param string $invoiceCode 送货编号 快递公司对应的拼音
      * @param string $invoiceName 送货公司的名称
-     * @param string $invoiceNo 送货单号
-     * @return boolean
+     * @param string $invoiceNo   送货单号
+     *
+     * @return bool
+     *
      * @author sunanzhi <sunanzhi@hotmail.com>
      */
-    public function editOrderRefundInvoice(int $oiId, int $orderId, string $invoiceCode, string $invoiceName, string $invoiceNo):bool;
-    
+    public function editOrderRefundInvoice(int $oiId, int $orderId, string $invoiceCode, string $invoiceName, string $invoiceNo): bool;
 
     /**
-     * 获取退货信息
-     * 
+     * 获取退货信息.
+     *
      * > 返回数据说明
      *
      * key | type |  value
@@ -595,7 +623,8 @@ interface BuyerOrderInterface
      * invoice_no   | string       | 送货单号
      * status   | int       | 物流状态：0 未查到 1 配送中 2 已送达
      *
-     * @param integer $orderId 订单id
+     * @param int $orderId 订单id
+     *
      * @return array
      * @requestExample({"orderId":"5000100"})
      * @returnExample({
@@ -620,15 +649,16 @@ interface BuyerOrderInterface
      *      "created_time":"1525750592",
      *      "update_time":"2018-07-04 18:00:18"
      * })
-     * 
+     *
      * @author sunanzhi <sunanzhi@hotmail.com>
      */
-    public function getOrderRefundInvoice(int $orderId):array;
+    public function getOrderRefundInvoice(int $orderId): array;
 
     /**
-     * 返回最多退款金额 和 运费
+     * 返回最多退款金额 和 运费.
      *
-     * @param integer $orderId 订单id
+     * @param int $orderId 订单id
+     *
      * @return array
      *
      * @requestExample({"orderId":"5000100"})
@@ -636,35 +666,38 @@ interface BuyerOrderInterface
      *
      * @author sunanzhi <sunanzhi@hotmail.com>
      */
-    public function orderRefundMoney(int $orderId):array;
+    public function orderRefundMoney(int $orderId): array;
 
     /**
-     * 集赞不足人数提醒 (支付后22小时未集赞成功，则立刻发送提醒通知)
+     * 集赞不足人数提醒 (支付后22小时未集赞成功，则立刻发送提醒通知).
      *
      * @param int $limit 每次处理的数量
-     * @return  array
+     *
+     * @return array
      *
      * @requestExample({"limit":"50"})
      * @returnExample([{"orderId":"50001659","buyerId":"2108412","requireLikes":"1","payTime":"1531724066","likes":"0"},{"orderId":"50001706","buyerId":"2848170","requireLikes":"1","payTime":"1532066230","likes":null}])
      *
      * @author zhangyingdi<zhangyingdi@eelly.net>
+     *
      * @since 2018.08.28
      */
-    public function listOrderCollectionNotEnough(int $limit = 50):array;
+    public function listOrderCollectionNotEnough(int $limit = 50): array;
 
     /**
-     * 更新订单的消息通知标志
+     * 更新订单的消息通知标志.
      *
-     * @param int $orderId 订单id
+     * @param int $orderId    订单id
      * @param int $noticeFlag 消息通知标志
+     *
      * @return bool
      *
      * @requestExample({"orderId":"50001707", "noticeFlag":8})
      * @returnExample(true)
      *
      * @author zhangyingdi<zhangyingdi@eelly.net>
+     *
      * @since 2018.08.28
      */
-    public function updateOrderNoticeFlag(int $orderId, int $noticeFlag):bool;
-
+    public function updateOrderNoticeFlag(int $orderId, int $noticeFlag): bool;
 }
