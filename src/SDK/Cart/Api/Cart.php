ff51560d96674c039bd7f9a4ac29d0585c4a870f
sunanzhi <sunanzhi@hotmail.com>
2018-09-12 10:48:42 +0800

    修改注释

--- src/SDK/Cart/Api/Cart.php
+++ src/SDK/Cart/Api/Cart.php
@@ -370,6 +370,7 @@ class Cart implements CartInterface
      * close  | int     | 店铺状态 0=正常，1=禁售，2=店铺关闭，3=店铺挂起，4=店铺暂停营业
      * unit   | string  | 商品单位 例如：件
      * goodsImage | string | 商品图片
+     * uniqueId   | string | 购物车唯一标识
      * 
      * > data 数据说明
      * key | type | value
@@ -396,7 +397,7 @@ class Cart implements CartInterface
      * })
      * @returnExample({
      *      "storeInfo":{"isMix":"1","mixMoney":"100","mixNum":"2","storeQuantity":"1"},
-     *      "goodsInfo":{"ifShow":"2","close":"0","unit":"件","goodsImage":"G03/M08/00/11/small_usauiblsiiwhnf_dhjahi.jpg"},
+     *      "goodsInfo":{"ifShow":"2","close":"0","unit":"件","goodsImage":"G03/M08/00/11/small_usauiblsiiwhnf_dhjahi.jpg","uniqueId":"372f86e3539ef75e5b49f393e98decc7"},
      *      "data":[{
      *          "spec_id":"28738542",
      *          "goods_id":"5155477",
@@ -446,6 +447,7 @@ class Cart implements CartInterface
      * close  | int     | 店铺状态 0=正常，1=禁售，2=店铺关闭，3=店铺挂起，4=店铺暂停营业
      * unit   | string  | 商品单位 例如：件
      * goodsImage | string | 商品图片
+     * uniqueId   | string | 购物车唯一标识
      * 
      * > data 数据说明
      * key | type | value
@@ -472,7 +474,7 @@ class Cart implements CartInterface
      * })
      * @returnExample({
      *      "storeInfo":{"isMix":"1","mixMoney":"100","mixNum":"2","storeQuantity":"1"},
-     *      "goodsInfo":{"ifShow":"2","close":"0","unit":"件","goodsImage":"G03/M08/00/11/small_usauiblsiiwhnf_dhjahi.jpg"},
+     *      "goodsInfo":{"ifShow":"2","close":"0","unit":"件","goodsImage":"G03/M08/00/11/small_usauiblsiiwhnf_dhjahi.jpg","uniqueId":"372f86e3539ef75e5b49f393e98decc7"},
      *      "data":[{
      *          "spec_id":"28738542",
      *          "goods_id":"5155477",
