940225171b5b5c1adfa45a1d610fd816779bdcf1
zydZhang <568321490@qq.com>
2018-09-14 15:10:54 +0800

    Develop (#578)
    * 订单重构，立即发货
    
    * 订单重构，添加物流信息跟修改物流信息接口
    
    * 订单重构，商品库存回滚
    
    * 订单重构，添加订单仲裁记录

--- src/SDK/Order/Api/Arbitrate.php
+++ src/SDK/Order/Api/Arbitrate.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Eelly\SDK\Order\Api;
 
+use Eelly\SDK\EellyClient;
 use Eelly\SDK\Order\Service\ArbitrateInterface;
 
 /**
@@ -20,6 +21,39 @@ use Eelly\SDK\Order\Service\ArbitrateInterface;
  */
 class Arbitrate implements ArbitrateInterface
 {
+    /**
+     * 添加订单仲裁记录
+     *
+     * @param array $data 订单仲裁数据
+     * @param int $data["orderId"] 订单id
+     * @param int $data["applyFlag"] 仲裁申请标志：1 买家申请 2 卖家申请
+     * @param string $data["phone"] 仲裁申请联系电话
+     * @param string $data["qq"] 仲裁申请联系QQ
+     * @param string $data["wechat"] 仲裁申请联系微信号
+     * @param int $data["buyerNumber"] 买家申请仲裁次数
+     * @param int $data["sellerNumber"] 卖家申请仲裁次数
+     * @param array $data["certificate"] 仲裁申请凭证
+     * @return bool
+     *
+     * @requestExample({"data":{"orderId":116,"applyFlag":1,"phone":"13430245645","qq":"","wechat":"","buyerNumber":1,"sellerNumber":0,"certificate":{}}})
+     * @returnExample(true)
+     *
+     * @author zhangyingdi<zhangyingdi@eelly.net>
+     * @since 2018.09.14
+     */
+    public function addOrderArbitrate(array $data):bool
+    {
+        return EellyClient::request('order/arbitrate', __FUNCTION__, true, $data);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function addOrderArbitrateAsync(array $data):bool
+    {
+        return EellyClient::request('order/arbitrate', __FUNCTION__, false, $data);
+    }
+
     /**
      * @return self
      */
