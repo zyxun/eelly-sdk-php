d513a62dd4ac77a0017b4abb8dd1fbe4173b989f
andy <568321490@qq.com>
2018-09-27 10:36:15 +0800

    订单重构，新增已打印订单接口

--- src/SDK/Order/Service/SellerOrderRefactoringInterface.php
+++ src/SDK/Order/Service/SellerOrderRefactoringInterface.php
@@ -815,4 +815,127 @@ interface SellerOrderRefactoringInterface
      * @since 2018.09.18
      */
     function extendReceiptTime(int $orderId, UidDTO $uidDTO):bool;
+
+    /**
+     * 搜索已打印订单.
+     *
+     * > 返回数据说明
+     *
+     * key         | type    | value
+     * ------------ |------  | --------
+     * current      | int    | 当前页码
+     * last         | int    | 下一页页码
+     * totalPages   | int    | 总页码
+     * totalItems   | int    | 数据总量
+     * items        | array  |当前数据
+     * items[]['orderId']     | string | 订单id
+     * items[]['ordern']      | string | 订单编号
+     * items[]['buyerName']   | string | 买家名
+     * items[]['orderStatus'] | int    | 订单状态
+     * items[]['orderAmount'] | int    | 实付(分)
+     * items[]initGoodsAmount | int    | 原货款(分)
+     * items[]initFreight     | int    | 原运款(分)
+     * items[]['freight']     | int    | 运费(分)
+     * items[]['createdDate'] | date | 订单日期
+     * items[]['ifMerge']     | bool | 是否有可合并订单
+     * items[]['productCount']| int  | 商品总件数
+     * items[]['ifRefund']    | int  | 订单是否申请过退货退款 (1:有 0:没有)
+     * items[]['goodsList']   | array | 商品列表
+     * items[]['goodsList'][]['goodsName']    | string | 商品名称
+     * items[]['goodsList'][]['price']        | int    | 商品价格(分)
+     * items[]['goodsList'][]['quantity']     | int    | 商品数量
+     * items[]['goodsList'][]['spec']         | string | 商品属性
+     * items[]['goodsList'][]['goodsImage']   | string | 商品图片
+     *
+     *
+     * @param int    $tab    订单筛选值  (0: 全部, 1: 待付款, 2: 待成团, 3: 待发货, 4: 待收货, 5: 待评价 6:退货退款 7:已取消)
+     * @param int    $page   第几页
+     * @param int    $limit  分页大小
+     * @param UidDTO $uidDTO uid dto
+     *
+     * @return array
+     *
+     * @author 肖俊明<xiaojunming@eelly.net>
+     * @author zhangyingdi<zhangyingdi@eelly.net>
+     *
+     * @requestExample({"tab":1, "page":2})
+     *
+     * @returnExample(
+     * {
+     *     "first": 1,
+     *     "before": 1,
+     *     "current": 1,
+     *     "last": 23,
+     *     "next": 2,
+     *     "totalPages": 23,
+     *     "totalItems": 45,
+     *     "limit": 2,
+     *     "items": [
+     *         {
+     *             "orderId": "160",
+     *             "orderSn": "1811370443",
+     *             "sellerName": "莫琼小店",
+     *             "osId": "26",
+     *             "likes": 2,
+     *             "evaluation": null,
+     *             "orderAmount": "2",
+     *             "freight": "1",
+     *             "createdTime": 1524555994,
+     *             "orderStatus": 8,
+     *             "createdDate": "2018-04-24",
+     *             "ifMerge": true,
+     *             "productCount": 100,
+     *             "goodsList": [
+     *                 {
+     *                     "ogId": "20000215",
+     *                     "orderId": "160",
+     *                     "goodsId": "1450168293",
+     *                     "gsId": "195022196",
+     *                     "price": "1",
+     *                     "quantity": "2",
+     *                     "goodsName": "【莫琼小店】 2018新款 针织衫\/毛衣  包邮",
+     *                     "goodsImage": "https:\/\/img03.eelly.test\/G02\/M00\/00\/03\/small_ooYBAFqzVV2ICEGRAAER2psay8IAAABggBWRl0AARHy759.jpg",
+     *                     "goodsNumber": "2",
+     *                     "spec": "颜色:如图色,尺码:均码",
+     *                     "createdTime": "1524555994",
+     *                     "updateTime": "2018-04-24 15:46:32"
+     *                 }
+     *             ]
+     *         },
+     *         {
+     *             "orderId": "159",
+     *             "orderSn": "1811370443",
+     *             "sellerName": "莫琼小店",
+     *             "osId": "26",
+     *             "likes": 0,
+     *             "evaluation": null,
+     *             "orderAmount": "2",
+     *             "freight": "1",
+     *             "createdTime": 1524550065,
+     *             "orderStatus": 7,
+     *             "createdDate": "2018-04-24",
+     *             "ifMerge": true,
+     *             "productCount": 100,
+     *             "goodsList": [
+     *                 {
+     *                     "ogId": "20000214",
+     *                     "orderId": "159",
+     *                     "goodsId": "1450168293",
+     *                     "gsId": "195022196",
+     *                     "price": "1",
+     *                     "quantity": "2",
+     *                     "goodsName": "【莫琼小店】 2018新款 针织衫\/毛衣  包邮",
+     *                     "goodsImage": "https:\/\/img03.eelly.test\/G02\/M00\/00\/03\/small_ooYBAFqzVV2ICEGRAAER2psay8IAAABggBWRl0AARHy759.jpg",
+     *                     "goodsNumber": "2",
+     *                     "spec": "颜色:如图色,尺码:均码",
+     *                     "createdTime": "1524550066",
+     *                     "updateTime": "2018-04-24 14:07:43"
+     *                 }
+     *             ]
+     *         }
+     *     ]
+     * }
+     * )
+     */
+    public function searchWaybillOrders(string $keywords = '', int $tab = 0, int $page = 1, int $limit = 20, UidDTO $uidDTO = null): array;
 }
