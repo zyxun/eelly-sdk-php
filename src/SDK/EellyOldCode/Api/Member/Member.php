2b3519f8e8adb2271e2b71718d8a38ba57eedb97
zyxun <542207975@qq.com>
2018-10-13 11:49:11 +0800

    oldcode发送验证码api

--- src/SDK/EellyOldCode/Api/Member/Member.php
+++ src/SDK/EellyOldCode/Api/Member/Member.php
@@ -52,4 +52,30 @@ class Member
     {
         return EellyClient::request('eellyOldCode/member/member', __FUNCTION__, true, $userId);
     }
+
+    /**
+     * 发送验证码
+     *
+     * @param string $mobile 手机号码|邮箱
+     * @param string $type   验证码类型
+     *                       手机注册：register
+     *                       手机找回密码：findPassword
+     *                       手机绑定：boundMobile
+     *                       手机修改密码：modifyPassword
+     *                       电子邮件修改密码：emailModifyPassword
+     *                       手机登陆店+: loginDianjia
+     *                       注册语音验证码： registerSendVoice
+     *                       快捷登录语音验证码 loginDianjiaSendVoice
+     *                       重置支付密码         resetPayPassword
+     *                       绑定银行卡        addBankCard
+     *
+     * @return array
+     *
+     * @author zhangyangxun
+     * @since 2018-10-13
+     */
+    public function createCaptcha(string $mobile, string $type)
+    {
+        return EellyClient::request('eellyOldCode/member/member', __FUNCTION__, true, $mobile, $type);
+    }
 }
