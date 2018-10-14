716cd5cf798f0c01a9670f3239d06e15eadcf4aa
runphp <runphp@qq.com>
2018-10-14 12:12:24 +0800

    补充文档

--- src/SDK/Order/Service/BuyerOrderInterface.php
+++ src/SDK/Order/Service/BuyerOrderInterface.php
@@ -55,8 +55,8 @@ interface BuyerOrderInterface
      *
      *  参数     | 类型 | 说明
      * -------- | -------- |-----
-     * goodsList | list  | 订单商品列表
-     * bizData   | map   | <http://runphp.net/api.eelly.com/order-status.html>
+     * goodsList | list  | 订单商品列表 参考订单详情 <order/buyerOrder/orderDetails>
+     * bizData   | map   | 订单业务数据 参考订单详情 <order/buyerOrder/orderDetails>
      *
      *
      * @param int          $tab                            订单状态筛选值(0: 全部订单 1: 待付款 2: 待分享 3: 待发货 4: 待收货 5: 待评价 6: 退货退款中 7: 交易完成 8: 已经取消)
@@ -224,7 +224,7 @@ interface BuyerOrderInterface
      *
      * key | type |  value
      * --- | ---- | -------
-     * orderStatus     | int      | 订单状态
+     * orderStatus     | int      | 订单状态   (改用bizData.bizCode业务编号)
      * consignee       | string   | 收货人姓名
      * mobile          | string   | 手机
      * regionName      | string   | 省市区
@@ -248,8 +248,9 @@ interface BuyerOrderInterface
      * productCount    | int       | 商品总件数
      * extension       | int       | 订单业务标识：0 普通订单  1 团购订单
      * expressStatus   | string    | 最新物流状态
-     * expressTime     | Datetime  | 最新物流时间
-     * countdown       | int       | 倒计时（秒），当orderStatus = 1 或 2 或 4 或 5
+     * expressTime     | Datetime  | 最新物流时间 （迁移至bizData.expressTime)
+     * countdown       | int       | 倒计时（秒），当orderStatus = 1 或 2 或 4 或 5    （迁移至bizData.countDown)
+     * payChannel      | int       | 支付渠道：0 线下 1 支付宝 2 微信钱包 3 QQ钱包 4 银联 5 移动支付  -1 未知(数据不存在)   2018-10-13 新增
      * timeList                       | array     | 时间列表
      * timeList['createdTime']        | int       | 下单时间
      * timeList['payTime']            | int       | 付款时间
@@ -262,21 +263,51 @@ interface BuyerOrderInterface
      * goodsList[]['quantity']     | int    | 商品数量
      * goodsList[]['spec']         | string | 商品属性
      * goodsList[]['goodsImage']   | string | 商品图片
+     * bizData                     | array  | 业务数据
+     * bizCode | int | 业务编号
+     * bizCode['orderStatus'] | int | 订单状态
+     * bizCode['title']              | string   | 订单状态标题
+     * bizCode['text']               |  string        | 订单状态描述
+     * bizCode['countDown']          | int |       倒计时(秒)
+     * bizCode['countDownTpl']       |string |  倒计时模板(模板变量 {time})
+     * bizCode['express']            |string |  快递信息
+     * bizCode['expressTime']        | int | 快递信息的时间戳(秒)
+     * bizCode['note']               | string | 留言
+     * bizCode['actions']            | list | 支持的操作<返回数据actions说明>
      *
-     * > 订单状态(orderStatus)
+     * > 业务编号(bizData.bizCode)
      *
-     * 值      |状态说明
-     * -------|----------
-     * 0      | 未知（错误值）
-     * 1      | 待付款
-     * 2      | 待分享
-     * 3      | 待发货
-     * 4      | 待收货
-     * 5      | 待评价
-     * 6      | 已评价
-     * 7      | 集赞失败,已退款
-     * 8      | 已退款, 交易取消
-     * 9      | 未付款, 交易取消
+     *    业务状态码(bizCode)  | 数据库状态码(orderStatus) | 状态说明               |title        |text              |countDownTpl                        |express           |actions         |actions说明
+     *    --------- |----------  | ----------------------|------------|----------------- | -----------------------------------|-----------------| ----------------|--------------
+     *    1         | **1**    | 等待付款                 |等待我付款      | 等待我付款        | 请在{time}内付款，逾期系统自动取消订单 | 无               | `cancel` `pay`  | `取消订单` `立即付款`
+     *    2         | **2**  order.extension = 0 | 等待卖家发货             |等待卖家发货      | 我已付款，等待卖家发货 | 无 | 卖家正在备货         | `refund` `notifySendGoods` | `申请退款` `提醒发货`
+     *    3        | **2**  order_count.likes < order.require_likes | 待成团               |待成团        | 待成团        | 无 | 无               | 无 | 无
+     *    4        | **2** order_count.likes >= order.require_likes | 集赞成功               |集赞成功      | 集赞成功        | 无 | 无               | `refund` `notifySendGoods` | `申请退款` `提醒发货`
+     *    5         | **4**    | 等待我收货              |等待我收货      | 卖家已发货，等待我收货 | 请在{time}内确认收货，逾期系统自动确认 | 最后一条物流动态 | `returnGoods` `expandReceivedTime` `confirmReceived` | `申请退货退款` `延长收货` `确认收货`
+     *    6        | (16, 20) order_refund.type = 1 and order.ship_time = 0 | 申请退款(未发货)         |申请退款中(未发货)      | 退款申请已提交，等待卖家处理 | {time}内卖家不处理，系统默认同意申请 | 无               | `agreedDetail` `refundDetail` | `查看协商记录` `退款详情`
+     *    7        | (16, 20) order_refund.type = 1 and order.ship_time > 0       | 申请退款(已发货)        |申请退款中(已发货)      | 退款申请已提交，等待卖家处理 | {time}内卖家不处理，系统默认同意申请 | 最后一条物流动态      | `agreedDetail` `refundDetail` | `查看协商记录` `退款详情`
+     *    8         | **11,12** order_evaluation null and order.return_flag = 0 | 交易完成(待评价)         |交易完成      | 待评价     | 无 | 最后一条物流动态      | `review`  | `去评价`
+     *    9         | **11,12** order_evaluation not null and order.return_flag = 0 | 交易完成(已评价)         |交易完成(买家收货/10天自动收)     | 交易完成    | 无 | 无               | 无 | 无
+     *    10         | **15** order.extension = 0 AND order_log.type = 0 | 交易取消(系统取消)       |交易取消      | 交易取消    | 因超时未付款，系统自动取消订单 | 无               | 无 | 无
+     *    11         | **15** order_log.type = 1 | 交易取消(买家取消)       |交易取消      | 交易取消    | 买家主动取消订单 | 无               | 无 | 无
+     *    12         | **15** order_log.type = 2 AND order.return_flag != 2 | 交易取消(卖家取消)       |交易取消      | 交易取消    | 卖家主动取消订单 | 无               | 无 | 无
+     *    13         | **15** order_log.type = 2 AND order.return_flag = 2 | 交易取消(全额退款)       |交易取消      | 交易取消    | 全额退款成功，订单取消 | 无               | `refundDetail` | `查看退款详情`
+     *    14 | **15** order.extension = 1 AND order_log.type = 0 | 交易取消(成团失败) |交易取消 | 交易取消 | 全额退款成功，订单取消 | 无 | `refundDetail` | `查看退款详情`
+     *    15        | (16, 20) order_refund.type = 2 | 申请退货退款           |申请退货中      | 退货申请已提交，等待卖家处理 | {time}内卖家不处理，系统默认同意申请 | 最后一条物流动态      | `agreedDetail` `refundDetail` | `查看协商记录` `查看退款详情`
+     *    16        | (17) | 同意退货              |等待我退货      | 卖家同意退货，等待我处理 | 您需在{time}内处理，逾期默认撤销退款申请 | 最后一条物流动态       | `cancelReturnGoods` `sendReturnGoods` | `撤销申请` `发出退货`
+     *    17        | (18)     | 已发退货             |等待卖家收货      | 我已发出退货，等待卖家收货 | 卖家需在{time}内确认收货，逾期自动收货 | 无               | `agreedDetail` `returnGoodsExpress` | `查看协商记录` `退货物流`
+     *    18        | (19) order_refund.type = 1 | 卖家拒绝退款           |卖家拒绝退款      | 卖家拒绝退款，等待我处理 | 您需在{time}内处理，逾期默认撤销退款申请 | 最后一条物流动态      | `agreedDetail` `queryRefund` | `查看协商记录` `去处理退款申请`
+     *    19        | (19) order_refund.type = 2 | 卖家拒绝退货           |卖家拒绝退货     | 卖家拒绝退货，等待我处理 | 您需在{time}内处理，逾期默认撤销退货申请 | 最后一条物流动态       | `agreedDetail` `queryReturnGoods` | `查看协商记录` `去处理退货申请`
+     *    20        | (21) order_refund.type = 1 | 卖家同意退款            |退款结算中(同意退款/2天自动同意)      | 退款结算中   | 卖家同意退款，衣联系统正在结算 | 无               | `refundDetail` | `查看退款详情`
+     *    21        | (21) order_refund.type = 2 | 收到退货             |退款结算中(卖家收货/10天自动收)      | 退款结算中   | 卖家确认收到退货，衣联系统正在结算 | 无               | `refundDetail` | `查看退款详情`
+     *    22        | (22) order_arbitrate.status is null or != 1 | 买家申请仲裁         |客服介入处理      | 我已申请衣联客服介入 | 客服会在3个工作日内介入处理，请耐心等待 | 最后一条物流动态       | `cancelArbitrate` | `撤销介入申请`
+     *    23        | (22) order_arbitrate.status = 1 | 客服介入中 |客服介入处理      | 衣联客服已介入处理，请耐心等待 | 客服会联系您了解情况，请保持联系方式畅通 | 最后一条物流动态       | 无 | 无
+     *    24        | (23) order_arbitrate.status is null or != 1 | 卖家申请仲裁         |客服介入处理      | 卖家申请衣联客服介入 | 客服会在3个工作日内介入处理，请耐心等待 | 最后一条物流动态       | 无 | 无
+     *    25        | **12**(25) order_arbitrate.blame_flag = 1 | 客服介入完成(钱给买家)  |交易完成      | 客服介入处理完毕 | 无 | 无               | `review`                                           | `待评价`
+     *    26        | **12**(25) order_arbitrate.blame_flag = 2 | 客服介入完成(钱给卖家)   |交易完成      | 客服介入处理完毕 | 无 | 无               | `review`                                           | `待评价`
+     *    27        |**12**(25) order_arbitrate.blame_flag = 3  | 客服介入完成(退一部分)   |交易完成      | 客服介入处理完毕 | 无 | 无               | `review` | `待评价`
+     *    28        |**12**(25) order_arbitrate.blame_flag = 4  | 客服介入完成(财务处理) |交易完成      | 客服介入处理完毕 | 其他具体原因 | 无               | `review` | `待评价`
+     *    29 | **12**(25) order_arbitrate is null | 退货退款交易完成(无平台介入) |交易完成 | 交易完成 | 无 | 无 | `review` | `待评价`
      *
      * @param int         $orderId 订单id
      * @param UidDTO|null $uidDTO  uid dto
@@ -400,9 +431,9 @@ interface BuyerOrderInterface
     /**
      * 确认收货.
      *
-     * @param int $orderId 订单id
-     * @param string $password 支付密码
-     * @param  UidDTO|null $uidDTO  uid dto
+     * @param int         $orderId  订单id
+     * @param string      $password 支付密码
+     * @param UidDTO|null $uidDTO   uid dto
      *
      * @return bool
      *
@@ -421,7 +452,7 @@ interface BuyerOrderInterface
      * @param UidDTO|null $uidDTO  uid dto
      *
      * @return bool
-     * 
+     *
      * @requestExample({"orderId":"160","memo":"你买了个表", "type":4})
      *
      * @returnExample(true)
