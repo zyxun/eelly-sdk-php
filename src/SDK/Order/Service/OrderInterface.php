3b9144a92542ee099a8dae0ec1d20d40c06cbd8e
runphp <runphp@qq.com>
2018-10-26 19:58:24 +0800

    文档修改

--- src/SDK/Order/Service/OrderInterface.php
+++ src/SDK/Order/Service/OrderInterface.php
@@ -658,7 +658,7 @@ interface OrderInterface
      * @since 2018-08-08
      */
     public function getUnshippedInfo(int $page = 1, int $limit = 10): array;
-    
+
     /**
      *  购物车确认下单列表
      *
@@ -680,18 +680,18 @@ interface OrderInterface
      * >quantity | int | 购买数量
      * >spId | int | 规格ID
      * > ---
-     * 
+     *
      * ### 返回结果
      * 字段名 | 类型 |描述
      * ---|---|---
      * defaultAddress | array | 默认收货地址
      * storeOrderGoods | array | 用户下单数据
      * freePostCard | array  |包邮卡信息
-     * 
+     *
      * >返回数据说明
-     * 
+     *
      * >defaultAddress 默认收货地址返回字段说明
-     * 
+     *
      * >字段名 | 类型 |描述
      * >-- | ---- | -----
      * >addrId | int | 收货地址id
@@ -700,7 +700,7 @@ interface OrderInterface
      * >detailInfo | string | 收货人详细地址
      * >default | int | 是否为默认收货地址
      * >regionId | int | 地区id
-     * 
+     *
      * >storeOrderGoods 字段说明
      * >字段名 | 类型 |描述
      * >-- | ---- | -----
@@ -712,7 +712,7 @@ interface OrderInterface
      * >goodsInfo | array | 商品信息
      * >couponInfo | array | 优惠券信息
      * >expressWay | array | 快递信息
-     * 
+     *
      *
      * >storeOrderGoods[]['goodsInfo'] 字段说明
      * >字段名 | 类型 |描述
@@ -725,7 +725,7 @@ interface OrderInterface
      * >goodsName | string | 商品名称
      * >specInfo | array | 规格信息
      * >priceData | array | 价格信息
-     * 
+     *
      * >storeOrderGoods[]['goodsInfo']['specInfo'] 字段说明
      * >字段名 | 类型 |描述
      * >-- | ---- | -----
@@ -741,18 +741,18 @@ interface OrderInterface
      * >-- | ---- | -----
      * >goodsId | int | 商品id
      * >storeId | int | 店铺id
-     * >priceType | int | 价格类型 
+     * >priceType | int | 价格类型
      * >priceLower | float | 最低价
      * >priceUpper | float | 最高价
      * >unitPrice | float | 单价
      * >priceData  | array | 起批价格区间
-     * 
+     *
      * >storeOrderGoods[]['priceData']['priceDetail'] 字段说明
      * >字段名 | 类型 |描述
      * >-- | ---- | -----
      * >actId | int | 活动id
      * >goodsId | int | 商品id
-     * >price | int | 价格 
+     * >price | int | 价格
      * >tag | float | 活动标题
      *
      * >storeOrderGoods[]['priceData']['priceData'] 字段说明
@@ -780,16 +780,16 @@ interface OrderInterface
      * >weight  | float | 重量
      * >expressType | int | 是否可以到付 (0.支付 1.不支持)
      * >expressSelect | int | 快递类型 (1：货运；2：快递；3：EMS)
-     * 
+     *
      * >返回数据说明
      * >freePostCard 默认收货地址返回字段说明
-     * 
+     *
      * >字段名 | 类型 |描述
      * >-- | ---- | -----
      *
      *
-     *  
-     * 
+     *
+     *
      * @param array $data 请求参数
      * @param array $data['isFrom'] 下单平台 1.pc 2.wap 3.app
      * @param array $data['type'] 下单类型 1.购物车下单 2.立即下单
@@ -799,11 +799,11 @@ interface OrderInterface
      * @param int $data['goods'][0]['spec']     选中的商品规格
      * @param int $data['goods'][0]['isSpelling'] 是否拼团商品
      * @param array $data['uniqueIds'][] 购物车商品主键id (购物车下单必传,立即下单传空)
-     * 
+     *
      * @returnExample({"defaultAddress":{"addrId":"547627","userName":"黄丽玲","telNumber":"18312019106","detailInfo":"河北省 秦皇岛市 市辖区 啦咯啦咯啦咯啦咯啦","default":"1","regionId":"130301"},"storeOrderGoods":[{"storeId":"1760244","storeName":"女装大大","creditValue":{"type":1,"number":1},"totalQuantity":12,"totalWeight":1,"goodsInfo":[{"goodsId":5578939,"totalPrice":0.12,"goodsCount":12,"goodsNumber":"","goodsImage":"https:\/\/img01.eelly.test\/G01\/M00\/00\/06\/small_oYYBAFtMOZWIGOgHAAGL_sMz2wAAAACagKbTqgAAYwW358.jpg","goodsName":"运费","specInfo":[{"specId":"32090865","price":"0.01","originalPrice":"0.00","quantity":12,"color":"如图色","size":"均码","stock":99978}],"priceData":{"goodsId":"5578939","storeId":"1760244","priceType":2001,"priceLower":"0.01","priceUpper":"0.01","priceData":[{"lowerLimit":"1","upperLimit":"0","price":"0.01","type":"1"}],"pricePay":"0.01","priceTitle":"限时特惠","priceDetail":{"actId":"3401","goodsId":"5578939","nums":"0","mbrBuyLimit":"0","price":"0.01","typeInfo":"a:0:{}","tag":"限时特惠","startTime":"1503561600","endTime":"1542441599","type":"16","isLimitMbrbuy":"1","single":"0","isSetNums":"1","expireTime":4196480},"unitPrice":"0.01"}}],"couponInfo":[{"couponId":"1450168327","couponNo":"1450168327SS","startTime":"1525329993","endTime":"1525329993","recId":"111"}],"totalPrice":0.12,"expressWay":[{"name":"货运","shippingId":"222789","expressType":"0","express_select":"1","freight":"0","weight":"0"},{"name":"运费到付","shippingId":"222789","expressType":"1","expressSelect":"1","freight":"0","weight":"0"}],"defaultAddress":{"addrId":"547627","userName":"黄丽玲","telNumber":"18312019106","detailInfo":"河北省 秦皇岛市 市辖区 啦咯啦咯啦咯啦咯啦","default":"1","regionId":"130301"},"fullSendActiveConfig":["暂时给个"]}],"freePostCard":["暂时给个"]})
-     * 
+     *
      * @param UidDTO $user
-     * 
+     *
      * @author wechan
      * @since 2018年08月22日
      */
@@ -833,10 +833,10 @@ interface OrderInterface
      * @since 2018-08-24
      */
     public function getLastOrderTime(array $condition): array;
-    
+
     /**
      * 订单下单接口
-     * 
+     *
      * @param array $data 订单信息
      * @param array $data['orderInfo'] 商品信息
      * @param int $data['orderInfo'][0]['storeId']  店铺ID
@@ -854,35 +854,35 @@ interface OrderInterface
      * @param int $data['userId'] 用户id
      * @param int $data['fromFlag'] 0 PC 1 WAP 2 店+APP 3 衣联小程序 4 快应用 5 联美小程序 6 市场小程序
      * @param int $data['isSpelling']  是否拼团订单
-     * 
+     *
      * @param UidDTO $user 登录用户信息
-     * 
+     *
      * @return array
-     * 
+     *
      * @returnExample({"50002027", "50002026"})
-     * 
+     *
      * @author wechan
      * @since 2018年09月04日
      */
     public function saveMallOrder(array $data, UidDTO $user = null): array;
-    
+
     /**
      * 获取快递方式和运费价格
-     * 
+     *
      * @param array $goods 购物车商品列表
      * @param int $goods[0]['goodsId']  选中的商品ID
      * @param int $goods['spec']     选中的商品规格
      * @param int $goods['spec'][0]['quantity']    选中的商品规格
      * @param int $goods['spec'][0]['specId']    选中的商品规格
      * @param int $regionId 地区id
-     * 
+     *
      * @returnExample([{"name":"货运","shippingId":"222789","expressType":0,"express_select":"1","freight":0,"weight":0},{"name":"运费到付","shippingId":"222789","expressType":"1","expressSelect":"1","freight":0,"weight":0}])
-     * 
+     *
      * @author wechan
      * @since 2018年08月23日
      */
     public function getExpressWay(array $goods, int $regionId): array;
-    
+
     /**
      * 根据传过来的订单id，返回对应可以使用的优惠卷列表
      *
@@ -891,12 +891,12 @@ interface OrderInterface
      * @param int   $userId  用户id
      *
      * @returnExample([{"couponId": 1450168327,"couponNo": "1450168327SS","startTime":1525329993,"endTime":1525329993,"recId":111}])
-     * 
+     *
      * @return array
      *
      */
     public function getOrderCouponList(float $money, int $storeId, int $userId):array;
-    
+
     /**
      * 订单发起的支付.
      *
@@ -905,7 +905,7 @@ interface OrderInterface
      * @param string $platform  支付平台: app:手机app pc:电脑pc端 wap:手机wap端 smallWechat:小程序
      * @param array $extend 扩展信息,比如某宝账号,某小程序账号信息。
      * @param UidDTO $user      登录用户信息
-     * 
+     *
      * ### 返回参数说明
      * 字段名 | 类型 |描述
      * -- | ---- | -----
@@ -914,8 +914,8 @@ interface OrderInterface
      * data | array | 第三方支付(余额支付)返回的结果
      * orderSns  | array | 订单号
      * orderIds | array | 订单id
-     * 
-     * 
+     *
+     *
      * > app支付宝:
      *
      *  `{"platform":"alipayApp","billNo":"201809280150456160","data":{"alipay_sdk":"lokielse\/omnipay-alipay","app_id":"2016122204515132","biz_content":"{\"out_trade_no\":\"201809280150456160\",\"total_amount\":200,\"subject\":\"\\u8ba2\\u5355\\u652f\\u4ed8\",\"body\":\"\",\"goods_type\":1,\"passback_params\":\"paId%3D12640%26userId%3D1760467%26account%3D126mail.wap%26type%3D1%26transact%3D2%26platform%3DalipayApp\",\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}","charset":"UTF-8","format":"JSON","method":"alipay.trade.app.pay","notify_url":"https:\/\/cs.blty.com\/alipay\/notifyV2.html","sign_type":"RSA","timestamp":"2018-09-28 14:00:56","version":"1.0","sign":"XAcLoq+N4upFq3tTo+7PrjXB9G+By9Gp\/6iyrThr5QbotjNX+8CEYxSgXl7DjwnLpRgewsMCb0tAoEtRuRls4OYmJFOdoVY5TaVpNA9sa6N72PkGP5dyJ8NFJUXitNtF31bnpYUg\/RGBqK8q7JdwRU9elzFPUSlPVJLN0jZ2VXQ=","order_string":"alipay_sdk=lokielse%2Fomnipay-alipay&app_id=2016122204515132&biz_content=%7B%22out_trade_no%22%3A%22201809280150456160%22%2C%22total_amount%22%3A200%2C%22subject%22%3A%22%5Cu8ba2%5Cu5355%5Cu652f%5Cu4ed8%22%2C%22body%22%3A%22%22%2C%22goods_type%22%3A1%2C%22passback_params%22%3A%22paId%253D12640%2526userId%253D1760467%2526account%253D126mail.wap%2526type%253D1%2526transact%253D2%2526platform%253DalipayApp%22%2C%22product_code%22%3A%22FAST_INSTANT_TRADE_PAY%22%7D&charset=UTF-8&format=JSON&method=alipay.trade.app.pay&notify_url=https%3A%2F%2Fcs.blty.com%2Falipay%2FnotifyV2.html&sign_type=RSA&timestamp=2018-09-28+14%3A00%3A56&version=1.0&sign=XAcLoq%2BN4upFq3tTo%2B7PrjXB9G%2BBy9Gp%2F6iyrThr5QbotjNX%2B8CEYxSgXl7DjwnLpRgewsMCb0tAoEtRuRls4OYmJFOdoVY5TaVpNA9sa6N72PkGP5dyJ8NFJUXitNtF31bnpYUg%2FRGBqK8q7JdwRU9elzFPUSlPVJLN0jZ2VXQ%3D"},"orderSns":["2153810322557191"],"orderIds":[50001781]}`
@@ -923,41 +923,49 @@ interface OrderInterface
      *  > app微信:
      *
      *  `{"platform":"wechatPayApp","billNo":"201809280152655879","data":{"appid":"wxdd557bb66b43f811","partnerid":"1329161001","prepayid":"wx28143736138251c8eb6862222820299484","package":"Sign=WXPay","noncestr":"040ef719374befe679a49cc7f0c5a66d","timestamp":1538116656,"outTradeNo":"201809280152655879","sign":"7F9279462A61B535190EC3E59C52F443"},"orderSns":["2153810322557191"],"orderIds":[50001781]}`
-     * 
+     *
      * > pc支付宝:
      *
      *  `{"platform":"alipayLegacyWeb","billNo":"201810080154962945","data":"https:\/\/mapi.alipay.com\/gateway.do?_input_charset=UTF-8&payment_type=1","orderSns":["2153812014142953"],"orderIds":[50001783]}`
-     * 
+     *
      * >pc微信:
      *
-     *  `{"platform":"wechatPayNative","billNo":"201810080150664981","data":"weixin:\/\/wxpay\/bizpayurl?pr=RVf4P5v","orderSns":["2153812014142953"],"orderIds":[50001783]}`
-     * 
+     *  ```json
+     * {
+     *     "platform":"wechatPayNative",
+     *     "billNo":"201810080150664981",
+     *     "data":"weixin:\/\/wxpay\/bizpayurl?pr=RVf4P5v",
+     *     "orderSns":["2153812014142953"],
+     *     "orderIds":[50001783]
+     * }
+     *  ```
+     *
      * @return array
-     * 
+     *
      *
      * @author wechan<pxs6216@dingtalk.com>
      *
      * @since 2018年09月10日
      */
     public function orderPay(array $orderIds, string $type = 'wechat', string $platform = 'pc', array $extend = [], UidDTO $user = null): array;
-    
+
     /**
      * 根据订单id 获取金额跟运费
-     * 
+     *
      * ### 返回结果
      * 字段名 | 类型 |描述
      * ---|---|---
      * totalFee | int | 总运费(分)
      * totalAmount | int | 总金额(分)
      * isSetPayPass | int | 是否设置支付密码 1.已设置 0.未设置
-     * 
+     *
      * @param array $orderIds 订单id
-     * 
+     *
      * @return array
-     * 
+     *
      * @returnExample({"totalFee":1,"totalAmount":1,"isSetPayPass":1})
-     * 
-     * 
+     *
+     *
      * @author wechan
      * @since 2018年9月27日
      */
@@ -973,24 +981,24 @@ interface OrderInterface
      * @since 2018-10-09
      */
     public function getOrderSumByCondition(array $conditions): array;
-    
+
     /**
      * 根据条件获取订单信息
-     * 
+     *
      * @param string $conditions 订单条件
      * @param array $bind 绑定数据
      * @param array $extend 扩展信息
-     * 
+     *
      * @author wechan
      * @since 2018年10月10日
      */
     public function getOrderConditionInfo(string $conditions = "", array $bind = [], array $extend = []):array;
-    
+
     /**
      * 回调订单支付.
      *
      * @param array $relData 支付请求数据
-     * 
+     *
      * [
      *    "userId" => 148086 用户id
      *    "storeId" => 148086 店铺id
