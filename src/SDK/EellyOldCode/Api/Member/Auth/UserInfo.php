5b1edc8fe47fd0d57322270b64b38e5b7107527a
sunanzhi <sunanzhi@hotmail.com>
2018-10-29 14:43:42 +0800

    新增获取认证信息的接口

---
+++ src/SDK/EellyOldCode/Api/Member/Auth/UserInfo.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\EellyOldCode\Api\Member\Auth;
+
+use Eelly\SDK\EellyClient;
+
+/**
+ * Class EnterpriseAuth.
+ *
+ * var/eelly-old-code/modules/Member/Service/MemberService.php
+ * 
+ * @author sunanzhi <sunanzhi@hotmail.com>
+ */
+class UserInfo
+{
+    /**
+     * 获取认证信息
+     *
+     * @param integer $userId 用户id
+     * @return array
+     * 
+     * @author sunanzhi <sunanzhi@hotmail.com>
+     * @since 2018.10.29
+     */
+    public function getAuthAuditStatus(int $userId)
+    {
+        return EellyClient::request('eellyOldCode/member/Auth/UserInfo', __FUNCTION__, true, $userId);
+    }
+}
