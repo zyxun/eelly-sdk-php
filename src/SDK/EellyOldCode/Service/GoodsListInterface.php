8aaf6882c3cee72c1887ff9129c025aff763225e
runphp <runphp@qq.com>
2018-11-01 10:41:04 +0800

    新增参数

--- src/SDK/EellyOldCode/Service/GoodsListInterface.php
+++ src/SDK/EellyOldCode/Service/GoodsListInterface.php
@@ -20,13 +20,30 @@ interface GoodsListInterface
     /**
      * 获取卖家的商品列表.
      *
+     * > 排序字段说明
+     *
+     * 字段 | 说明
+     * ---- | ------
+     * `goods_id`       | 商品id(等于商品发布时间)
+     * `price`          | 价格
+     *  `last_show_time`  | 上次上架时间
+     * `wait_show_time`   |  距待上架时间
+     * `sales`       |    商品销量(有水)
+     * `stock`       |   库存
+     * `sale_total`  |    真实商品销售数量
+     * `pv`          |    商品pv
+     *
+     *
      * @param int    $cateId   分类id
+     * @param int    $publishType 发布类型 0:全部 1:完整版上款商品 2:快速上款商品
      * @param string $keywords 搜索关键词
+     * @param string $orderBy  排序方式('goods_id desc'或'goods_id asc')字段参考`排序字段说明`
      * @param int    $page     第几页
      * @param int    $limit    分页大小
+     *
      * @param UidDTO $uidDTO   uid dto
      *
      * @return array
      */
-    public function sellerPageForChat(int $cateId = 0, string $keywords = '', int $page = 1, int $limit = 100, UidDTO $uidDTO = null): array;
+    public function sellerPageForChat(int $cateId = 0, int $publishType = 0, string $keywords = null, string $orderBy = 'goods_id desc', int $page = 1, int $limit = 100, UidDTO $uidDTO = null): array;
 }
