59e1c57b533bec4f3946f3e5064654b44d428ba8
runphp <runphp@qq.com>
2018-11-01 09:56:11 +0800

    修改参数

--- src/SDK/EellyOldCode/Service/GoodsListInterface.php
+++ src/SDK/EellyOldCode/Service/GoodsListInterface.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace Eelly\SDK\EellyOldCode\Service;
 
+use Eelly\DTO\UidDTO;
+
 interface GoodsListInterface
 {
     /**
@@ -22,8 +24,9 @@ interface GoodsListInterface
      * @param string $keywords 搜索关键词
      * @param int    $page     第几页
      * @param int    $limit    分页大小
+     * @param UidDTO $uidDTO   uid dto
      *
      * @return array
      */
-    public function sellerPageForChat(int $cateId, string $keywords, int $page = 1, int $limit = 100): array;
+    public function sellerPageForChat(int $cateId = 0, string $keywords = '', int $page = 1, int $limit = 100, UidDTO $uidDTO = null): array;
 }
