c4c59ea66456b2d106759bd32a6b49b7fdb3c821
runphp <runphp@qq.com>
2018-11-05 11:57:44 +0800

    新增参数 page limit

--- src/SDK/Live/Service/LiveOrderInterface.php
+++ src/SDK/Live/Service/LiveOrderInterface.php
@@ -28,21 +28,25 @@ interface LiveOrderInterface
      *
      * @param int         $liveId 直播id
      * @param int         $type   订单类型(1 待付款 2 已付款)
+     * @param int         $page   分页
+     * @param int         $limit  分页大小
      * @param UidDTO|null $uidDTO uid dto
      *
      * @return array
      *
      * @author hehui<hehui@eelly.net>
      */
-    public function myAppletLiveOrders(int $liveId, int $type, UidDTO $uidDTO = null): array;
+    public function myAppletLiveOrders(int $liveId, int $type, int $page = 1, int $limit = 20, UidDTO $uidDTO = null): array;
 
     /**
      * 获取直播间直播期间的小程序订单信息.
      *
      * @param int $liveId 直播id
-     * @param int $type  订单类型(1 待付款 2 已付款)
+     * @param int $type   订单类型(1 待付款 2 已付款)
+     * @param int         $page   分页
+     * @param int         $limit  分页大小
      *
      * @return array
      */
-    public function appletLiveOrders(int $liveId, int $type): array;
+    public function appletLiveOrders(int $liveId, int $type, int $page = 1, int $limit = 20): array;
 }
