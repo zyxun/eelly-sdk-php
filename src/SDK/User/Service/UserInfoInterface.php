2bc1481c5c27ea5b30ffd0dbc8fd3b1d94e2ff21
zyxun <542207975@qq.com>
2018-11-21 14:09:25 +0800

    获取申请提现的用户的信息

---
+++ src/SDK/User/Service/UserInfoInterface.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\User\Service;
+
+/**
+ * Interface UserInfoInterface.
+ *
+ * @author hehui<runphp@dingtalk.com>
+ */
+interface UserInfoInterface
+{
+    /**
+     * 获取一条用户信息.
+     *
+     * @param int $userId 用户id
+     *
+     * @return array
+     *
+     *@author hehui<runphp@dingtalk.com>
+     */
+    public function getOne(int $userId): array;
+
+    /**
+     * 获取多条用户信息.
+     *
+     * @param array $userIds 用户id列表
+     *
+     * @return array
+     *
+     * @author hehui<runphp@dingtalk.com>
+     */
+    public function getList(array $userIds): array;
+
+    /**
+     * 获取申请提现的用户的信息
+     *
+     * @param array $userIds
+     * @return array
+     *
+     * @author zhangyangxun
+     * @since 2018-11-21
+     */
+    public function getWithdrawUserInfo(array $userIds): array ;
+}
