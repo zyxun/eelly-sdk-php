ff0c1c7ba6043d85230f074f0b03041e30cd84ed
sunanzhi <sunanzhi@hotmail.com>
2018-12-20 10:49:24 +0800

    新增用户提现列表V2接口

--- src/SDK/Pay/Service/WithdrawInterface.php
+++ src/SDK/Pay/Service/WithdrawInterface.php
@@ -398,4 +398,66 @@ interface WithdrawInterface
      * @since 2018.11.28
      */
     public function getWithdrawListForPc(int $storeId, array $params = [], int $page = 1, int $limit = 50, UidDTO $user = null):array;
+
+    /**
+     * 用户获取提现列表V2
+     *
+     * > conditions 必须参数内容
+     * 
+     * key | type | value
+     * --- | ---- | -----
+     * limit | int | 可选 默认 10
+     * startTime | int | 必须 开始搜索时间 (首页服务端默认获取当前时间，请求下页时要传当前页面最后一个数据的startTime)
+     * 
+     * > 返回数据说明
+     * 
+     * key | type | value
+     * --- | ---- | ----
+     * allMoney     | int    | 单月提现总额
+     * month        | string | 年月份
+     * allMoneyV2   | float  | 当前月份的提现总额 元单位
+     * startTime    | string | 申请下页所需参数
+     * data         | array  | 当前月数据
+     * 
+     * > data 数据说明
+     * 
+     * key | type | value
+     * --- | ---- | ----
+     * pwId         | int    | 申请提现id
+     * paId         | int    | 资金账号id
+     * commissionRatio | float  | 提现手续费率
+     * money           | int    | 提现金额
+     * gbCode          | int    | 银行地区id
+     * bankId          | int    | 提现银行id
+     * bankName        | string | 提现银行名称
+     * bankSubbranch   | string | 支行名称
+     * billNo          | string | 交易号
+     * thirdNo         | string | 第三方交易号(支付宝/微信/银联)
+     * status          | int    | 处理状态：0 未处理 1 成功 2 失败 3 处理中
+     * remark          | string | 备注
+     * adminRemark     | string | 系统及管理员备注
+     * handleTime      | int    | 处理时间
+     * createdTime     | int    | 创建时间
+     * updateTime      | string | 更新时间
+     * handling        | int    | 手续费
+     * 
+     * @param integer $userId 当前登陆的账号id
+     * @param integer $storeId 店铺id 0:店+ 非0:厂家
+     * @param integer $page 分页页码 默认 1
+     * @param integer $limit 数据单页数量
+     * @return array
+     * 
+     * @requestExample({"userId":"148086","storeId":"148086","page":"1","limit":"50"})
+     * @returnExample({"allMoney":"1899","month":"2018-9","data":[{"pwId":"35","paId":"2","commissionRation":"0.008","money":"129","gbCode":"0","bankId":"186","bankName":"微信","bankSubbranch":"","bankAccount":"oW-yJwJS5QW2mb-JvNtwbHcgpk74","billNo":"201809040261074056","thirdNo":"","status":"1","remark":"用户微信提现1.30元","adminRemark":"提现成功","handleTime":"1536051543","createdTime":"1536051543","updateTime":"2018-09-04 16:59:10","handling":"1.0"}]})
+     * 
+     * 
+     * @param integer $userId 用户id
+     * @param integer $storeId 店铺id
+     * @param array $conditions 筛选条件
+     * @return array
+     * 
+     * @author sunanzhi <sunanzhi@hotmail.com>
+     * @since 2018.12.20
+     */
+    public function getWithdrawListV2(int $userId, int $storeId, array $conditions):array;
 }
