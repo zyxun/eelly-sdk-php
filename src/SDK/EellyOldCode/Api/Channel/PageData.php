f606aca37d3cfb3d7ef1bc299181b106514b5079
zyxun <542207975@qq.com>
2018-12-21 15:05:08 +0800

    后台配置二维码数据接口

---
+++ src/SDK/EellyOldCode/Api/Channel/PageData.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\EellyOldCode\Api\Channel;
+
+use Eelly\SDK\EellyClient;
+
+/**
+ * Class PageDataService.
+ *
+ * modules/Channel/Service/PageDataService.php
+ *
+ * @author zhangyangxun
+ */
+class PageData
+{
+    /**
+     * 获取ecm_home_page数据
+     *
+     * @param string $title
+     * @param int    $typeId
+     * @param bool   $isDecode
+     * @return mixed
+     */
+    public function getHomeData(string $title, int $typeId = 20, $isDecode = true)
+    {
+        return EellyClient::request('eellyOldCode/Channel/PageData', __FUNCTION__, true, $title, $typeId, $isDecode);
+    }
+}
