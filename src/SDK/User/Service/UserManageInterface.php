1046a121c688953e4005754c55788cd896f24168
zyxun <542207975@qq.com>
2018-12-27 11:58:04 +0800

    后台解绑手机前获取账号信息和操作验证串

--- src/SDK/User/Service/UserManageInterface.php
+++ src/SDK/User/Service/UserManageInterface.php
@@ -47,4 +47,20 @@ interface UserManageInterface
      * @internal
      */
     public function unbindUserMobile(int $userId, string $mobile, string $confirmCode = ''): bool;
+
+    /**
+     * 解绑手机前获取账号信息和操作验证串
+     *
+     * @param int    $userId
+     * @param string $mobile
+     * @return array
+     *
+     * @returnExample({"hasPassword":true, "isBindQQ":false, "isBindWechat":false, "confirmCode":"5cd747335b5c1f10f53ce0f8f913a6db"})
+     *
+     * @author zhangyangxun
+     * @since 2018-12-26
+     *
+     * @internal
+     */
+    public function checkUnbindMobile(int $userId, string $mobile): array;
 }
