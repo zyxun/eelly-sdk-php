b12679ff68b099a9aa4b9aa6f5605148b3e87bf0
zyxun <542207975@qq.com>
2019-03-13 11:44:51 +0800

    保存云信IM消息

---
+++ src/SDK/Im/Api/Msg.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\Im\Api;
+
+use Eelly\SDK\EellyClient;
+use Eelly\SDK\Im\Service\MsgInterface;
+
+/**
+ *
+ * @author shadonTools<localhost.shell@gmail.com>
+ */
+class Msg implements MsgInterface
+{
+    /**
+     * 保存IM消息
+     *
+     * @param array $msg
+     * @return mixed
+     *
+     * @author zhangyangxun
+     * @since 2019-03-13
+     */
+    public function saveNeteaseMsg(array $msg)
+    {
+        return EellyClient::request('im/msg', 'saveNeteaseMsg', true, $msg);
+    }
+
+    /**
+     * 保存IM消息
+     *
+     * @param array $msg
+     * @return mixed
+     *
+     * @author zhangyangxun
+     * @since 2019-03-13
+     */
+    public function saveNeteaseMsgAsync(array $msg)
+    {
+        return EellyClient::request('im/msg', 'saveNeteaseMsg', false, $msg);
+    }
+
+    /**
+     * @return self
+     */
+    public static function getInstance(): self
+    {
+        static $instance;
+        if (null === $instance) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+}
