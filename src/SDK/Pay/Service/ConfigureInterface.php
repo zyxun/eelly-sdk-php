3c5a39dad9765823094aa1efea605ce943bd511c
wechan <a421083@qq.com>
2019-03-22 14:40:55 +0800

    支付账号切换

---
+++ src/SDK/Pay/Service/ConfigureInterface.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * PHP version 5.5
+ *
+ * @copyright  Copyright (c) 2012-2015 EELLY Inc. (http://www.eelly.com)
+ * @link       http://www.eelly.com
+ * @license    衣联网版权所有
+ */
+
+
+namespace Eelly\SDK\Pay\Service;
+
+
+/**
+ * 收款帐户配置.
+ *
+ */
+interface ConfigureInterface
+{
+    /**
+     * 根据渠道类型返回对应类型的所有信息
+     * 
+     * @param int $channelType 渠道类型
+     * 
+     * @author wechan
+     * @since 2019年03月22日
+     */
+    public function getConfigureByChannelType(int $channelType): array;
+}
