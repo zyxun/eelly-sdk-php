4a25a85ee319e0819bb6c5fab970ef1ccb870c6b
andy <568321490@qq.com>
2019-04-01 17:43:52 +0800

    删除无用接口

--- src/SDK/Live/Api/Live.php
+++ src/SDK/Live/Api/Live.php
@@ -1605,35 +1605,7 @@ class Live implements LiveInterface
     {
         return EellyClient::request('live/live', 'checkUseCacheLiveRoomInfo', false, $liveId);
     }
-
-    /**
-     * 直播中的积分排行榜
-     *
-     * @return array
-     *
-     * @returnExample([{"ranking":1,"storeName":"\u7a88\u7a95\u8863\u8272","score":"320"},{"ranking":2,"storeName":"test\u5e97\u94fa\u6d4b\u8bd5","score":"124"}])
-     * @author zhangyingdi<zhangyingdi@eelly.net>
-     * @since 2019.03.28
-     */
-    public function listLiveScore(): array
-    {
-        return EellyClient::request('live/live', 'listLiveScore', true);
-    }
-
-    /**
-     * 直播中的积分排行榜
-     *
-     * @return array
-     *
-     * @returnExample([{"ranking":1,"storeName":"\u7a88\u7a95\u8863\u8272","score":"320"},{"ranking":2,"storeName":"test\u5e97\u94fa\u6d4b\u8bd5","score":"124"}])
-     * @author zhangyingdi<zhangyingdi@eelly.net>
-     * @since 2019.03.28
-     */
-    public function listLiveScoreAsync()
-    {
-        return EellyClient::request('live/live', 'listLiveScore', false);
-    }
-
+    
     /**
      * 根据传过来的直播id，返回对应的积分组成细节
      *
