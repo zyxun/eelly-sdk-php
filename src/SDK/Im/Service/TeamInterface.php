782596e97f273d31c54ea489a2d55e80ea14ef99
wechan <a421083@qq.com>
2019-04-18 17:28:36 +0800

    xxxxxxxx

--- src/SDK/Im/Service/TeamInterface.php
+++ src/SDK/Im/Service/TeamInterface.php
@@ -26,7 +26,19 @@ interface TeamInterface
      * @param int $page 分页
      * @param int $limit 每页显示数量, 默认20
      * 
-     * @returnExample({"groupMemberCount":0,"fansGroupList":[{"groupTitle":"bobo1972-官方直播①群","joinGroupCondition":"潜规则","isFull":0}]})
+     * @returnExample({"totalMember":0,"teams":[{"tid":"123","logo":"https:\/\/img09.eelly.com","title":"bobo1972-官方直播①群","subTitle":"潜规则","status":0}]})
+     * 
+     * ### 返回数据说明
+     *
+     * 字段|类型|说明
+     * --------|-------|--------------
+     * totalMember | int | 已进群人数
+     * teams | array | 群列表
+     * teams[]['tid'] | int | 群id
+     * teams[]['logo'] | string | 群头像
+     * teams[]['title'] | string | 标题
+     * teams[]['subTitle'] | string | 进群条件
+     * teams[]['status'] | int | 状态: 0:未进群 1:已进群 2:人满
      * 
      * @author wechan
      * @since 2019年04月18日
