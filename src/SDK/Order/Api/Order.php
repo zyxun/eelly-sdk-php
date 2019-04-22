801e73b2dd4fadf044e14a0f49af0ccbd33a4a43
wechan <a421083@qq.com>
2019-04-22 20:10:25 +0800

    xxxxxxxx

--- src/SDK/Order/Api/Order.php
+++ src/SDK/Order/Api/Order.php
@@ -2647,6 +2647,38 @@ class Order
     }
 
     /**
+     * 获取买家在指定店铺下的支付金额
+     *
+     * @param array $buyerIds 买家id
+     * @param int $storeId 店铺id
+     *
+     * @return array
+     *
+     * @author wechan
+     * @since  2019年04月22日
+     */
+    public function getBuyerStorePayAmount(array $buyerIds, int $storeId): array
+    {
+        return EellyClient::request('order/order', 'getBuyerStorePayAmount', true, $buyerIds, $storeId);
+    }
+
+    /**
+     * 获取买家在指定店铺下的支付金额
+     *
+     * @param array $buyerIds 买家id
+     * @param int $storeId 店铺id
+     *
+     * @return array
+     *
+     * @author wechan
+     * @since  2019年04月22日
+     */
+    public function getBuyerStorePayAmountAsync(array $buyerIds, int $storeId)
+    {
+        return EellyClient::request('order/order', 'getBuyerStorePayAmount', false, $buyerIds, $storeId);
+    }
+
+    /**
      * @return self
      */
     public static function getInstance(): self
