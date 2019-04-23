5187a6fa9b4c7a209ac14f585ac249b1a102d2fb
hehui <runphp@qq.com>
2019-04-23 14:20:05 +0800

    新增接口

---
+++ src/SDK/EellyOldCode/Api/UserContacts.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\EellyOldCode\Api;
+
+use Eelly\SDK\EellyClient;
+
+class UserContacts
+{
+    public static function addBlack(int $fromUid, int $fromType, int $toUid, int $toType): bool
+    {
+        return EellyClient::requestJson('eellyOldCode/userContacts', __FUNCTION__, [
+            'fromUid'  => $fromUid,
+            'fromType' => $fromType,
+            'toUid'    => $toUid,
+            'toType'   => $toType,
+        ]);
+    }
+
+    public static function unBlack(int $fromUid, int $fromType, int $toUid, int $toType): bool
+    {
+        return EellyClient::requestJson('eellyOldCode/userContacts', __FUNCTION__, [
+            'fromUid'  => $fromUid,
+            'fromType' => $fromType,
+            'toUid'    => $toUid,
+            'toType'   => $toType,
+        ]);
+    }
+}
