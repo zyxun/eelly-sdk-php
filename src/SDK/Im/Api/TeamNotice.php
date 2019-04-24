e29818226f04fe21cc19ca14b59dbc5aab36ac2a
hehui <runphp@qq.com>
2019-04-24 11:11:32 +0800

    新增接口

---
+++ src/SDK/Im/Api/TeamNotice.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eelly\SDK\Im\Api;
+
+use Eelly\SDK\EellyClient;
+
+class TeamNotice
+{
+    public static function getStorePrepareMsg(int $storeId): array
+    {
+        return EellyClient::requestJson('im/teamNotice', __FUNCTION__, ['storeId' => $storeId]);
+    }
+}
