6789bc911b26805285cbf6c81a62b8ef2d14cb44
hehui <runphp@qq.com>
2019-06-03 11:26:15 +0800

    修复代码错误

--- src/SDK/Order/Api/Relation.php
+++ src/SDK/Order/Api/Relation.php
@@ -1,11 +1,15 @@
-PhpParser\Node\Identifier {#1157
-  +name: "array"
-  #attributes: array:2 [
-    "startLine" => 26
-    "endLine" => 26
-  ]
-}
 <?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Eelly\SDK\Order\Api;
 
