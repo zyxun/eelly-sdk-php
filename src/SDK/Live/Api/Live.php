0641f5330b8e6f6b256469083818766c1358760a
andy <568321490@qq.com>
2019-06-04 15:27:39 +0800

    test

--- src/SDK/Live/Api/Live.php
+++ src/SDK/Live/Api/Live.php
@@ -1887,10 +1887,10 @@ class Live implements LiveInterface
             'endTime' => $endTime
         ]);
     }
-    
+
     public static function updateLiveFlag(int $liveId, int $flagVal, int $type = 0) : bool
     {
-        return EellyClient::requestJson('live/live', __FUNCTION__, array('liveId' => $liveId, 'flagVal' => $flagVal, 'type' => $type));
+        return EellyClient::requestJson('live/live', __FUNCTION__, ['liveId' => $liveId, 'flagVal' => $flagVal, 'type' => $type]);
     }
 
     /**
