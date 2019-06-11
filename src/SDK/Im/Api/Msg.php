6fd5f7bfbe36e4595c19086a92687a21eba39521
zyxun <542207975@qq.com>
2019-06-11 11:02:29 +0800

    send msg

--- src/SDK/Im/Api/Msg.php
+++ src/SDK/Im/Api/Msg.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 /*
  * This file is part of eelly package.
  *
@@ -14,21 +13,21 @@ declare(strict_types=1);
 namespace Eelly\SDK\Im\Api;
 
 use Eelly\SDK\EellyClient;
+use Eelly\SDK\Im\Service\MsgInterface;
 
 /**
+ *
  * @author shadonTools<localhost.shell@gmail.com>
  */
-class Msg
+class Msg implements MsgInterface
 {
     /**
-     * 保存IM消息.
+     * 保存IM消息
      *
      * @param array $msg
-     *
      * @return mixed
      *
      * @author zhangyangxun
-     *
      * @since 2019-03-13
      */
     public function saveNeteaseMsg(array $msg)
@@ -37,19 +36,43 @@ class Msg
     }
 
     /**
-     * 保存IM消息.
+     * 保存IM消息
      *
      * @param array $msg
-     *
      * @return mixed
      *
      * @author zhangyangxun
-     *
      * @since 2019-03-13
      */
     public function saveNeteaseMsgAsync(array $msg)
     {
         return EellyClient::request('im/msg', 'saveNeteaseMsg', false, $msg);
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param array $msg
+     * @return mixed
+     * @author zhangyangxun
+     * @since 2019/6/11
+     */
+    public function sendMsg(array $msg)
+    {
+        return EellyClient::request('im/msg', 'sendMsg', true, $msg);
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param array $msg
+     * @return mixed
+     * @author zhangyangxun
+     * @since 2019/6/11
+     */
+    public function sendMsgAsync(array $msg)
+    {
+        return EellyClient::request('im/msg', 'sendMsg', false, $msg);
     }
 
     /**
