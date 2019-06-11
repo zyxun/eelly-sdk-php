c5bcbf41bb766b7e3859c6c681e91c8dc23dbf9a
zyxun <542207975@qq.com>
2019-06-11 11:00:46 +0800

    send msg

--- src/SDK/Im/Service/MsgInterface.php
+++ src/SDK/Im/Service/MsgInterface.php
@@ -30,4 +30,14 @@ interface MsgInterface
      * @since 2019-03-13
      */
     public function saveNeteaseMsg(array $msg);
+
+    /**
+     * 发送消息
+     *
+     * @param array $msg
+     * @return mixed
+     * @author zhangyangxun
+     * @since 2019/6/11
+     */
+    public function sendMsg(array $msg);
 }
