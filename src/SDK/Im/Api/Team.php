46094f120ce08c6776a6be2a1de225dc4efdb70d
sunanzhi <sunanzhi@hotmail.com>
2019-06-17 10:58:01 +0800

    获取店铺可以激活的数据

--- src/SDK/Im/Api/Team.php
+++ src/SDK/Im/Api/Team.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 /*
  * This file is part of eelly package.
  *
@@ -18,13 +17,14 @@ use Eelly\SDK\Im\Service\TeamInterface;
 use Eelly\SDK\Im\Service\UidDTO;
 
 /**
+ *
  * @author shadonTools<localhost.shell@gmail.com>
  */
-class Team implements TeamInterface
+class Team
 {
     /**
-     * 获取主播粉丝群列表信息.
-     *
+     * 获取主播粉丝群列表信息
+     * 
      * ###返回数据说明
      *
      * 字段|类型|说明
@@ -36,16 +36,15 @@ class Team implements TeamInterface
      * teams[]['title'] | string | 标题
      * teams[]['subTitle'] | string | 进群条件
      * teams[]['status'] | int | 状态: 0:未进群 1:已进群 2:人满
-     *
-     * @param int         $storeId 店铺id
-     * @param int         $page    分页
-     * @param int         $limit   每页显示数量, 默认20
+     * 
+     * @param int $storeId 店铺id
+     * @param int $page 分页
+     * @param int $limit 每页显示数量, 默认20
      * @param UidDTO|null $uidDTO  登录用户
-     *
+     * 
      * @returnExample({"totalMember":0,"teams":[{"tid":"123","logo":"https:\/\/img09.eelly.com","title":"bobo1972-官方直播①群","subTitle":"潜规则","status":0}]})
-     *
+     * 
      * @author wechan
-     *
      * @since 2019年04月18日
      */
     public function getFansGroup(int $storeId, int $page = 1, int $limit = 20, UidDTO $uidDTO = null): array
@@ -54,8 +53,8 @@ class Team implements TeamInterface
     }
 
     /**
-     * 获取主播粉丝群列表信息.
-     *
+     * 获取主播粉丝群列表信息
+     * 
      * ###返回数据说明
      *
      * 字段|类型|说明
@@ -67,16 +66,15 @@ class Team implements TeamInterface
      * teams[]['title'] | string | 标题
      * teams[]['subTitle'] | string | 进群条件
      * teams[]['status'] | int | 状态: 0:未进群 1:已进群 2:人满
-     *
-     * @param int         $storeId 店铺id
-     * @param int         $page    分页
-     * @param int         $limit   每页显示数量, 默认20
+     * 
+     * @param int $storeId 店铺id
+     * @param int $page 分页
+     * @param int $limit 每页显示数量, 默认20
      * @param UidDTO|null $uidDTO  登录用户
-     *
+     * 
      * @returnExample({"totalMember":0,"teams":[{"tid":"123","logo":"https:\/\/img09.eelly.com","title":"bobo1972-官方直播①群","subTitle":"潜规则","status":0}]})
-     *
+     * 
      * @author wechan
-     *
      * @since 2019年04月18日
      */
     public function getFansGroupAsync(int $storeId, int $page = 1, int $limit = 20, UidDTO $uidDTO = null)
@@ -85,19 +83,18 @@ class Team implements TeamInterface
     }
 
     /**
-     * 买家进群事件.
-     *
-     * @param int    $tid    群id
+     * 买家进群事件
+     * 
+     * @param int $tid 群id
      * @param UidDTO $uidDTO 用户登录信息
-     *
+     * 
      * @returnExample({"result":1})
-     *
+     * 
      * 字段|类型|说明
      * --------|-------|--------------
      * result | int | 结果: 1.成功 0.失败
-     *
+     * 
      * @author wechan
-     *
      * @since 2019年04月19日
      */
     public function buyerJoinGroup(int $tid, UidDTO $uidDTO = null): array
@@ -106,19 +103,18 @@ class Team implements TeamInterface
     }
 
     /**
-     * 买家进群事件.
-     *
-     * @param int    $tid    群id
+     * 买家进群事件
+     * 
+     * @param int $tid 群id
      * @param UidDTO $uidDTO 用户登录信息
-     *
+     * 
      * @returnExample({"result":1})
-     *
+     * 
      * 字段|类型|说明
      * --------|-------|--------------
      * result | int | 结果: 1.成功 0.失败
-     *
+     * 
      * @author wechan
-     *
      * @since 2019年04月19日
      */
     public function buyerJoinGroupAsync(int $tid, UidDTO $uidDTO = null)
@@ -127,17 +123,14 @@ class Team implements TeamInterface
     }
 
     /**
-     * 账号拥有的群个数.
-     *
+     * 账号拥有的群个数
      * @internal
      *
      * @param int $userId
      * @param int $type
-     *
      * @return int
      *
      * @author zhangyangxun
-     *
      * @since 2019-04-23
      */
     public function getTeamNumOwnedInternal(int $userId, int $type): int
@@ -146,17 +139,14 @@ class Team implements TeamInterface
     }
 
     /**
-     * 账号拥有的群个数.
-     *
+     * 账号拥有的群个数
      * @internal
      *
      * @param int $userId
      * @param int $type
-     *
      * @return int
      *
      * @author zhangyangxun
-     *
      * @since 2019-04-23
      */
     public function getTeamNumOwnedInternalAsync(int $userId, int $type)
@@ -165,20 +155,19 @@ class Team implements TeamInterface
     }
 
     /**
-     * 买家进群事件.
+     * 买家进群事件
      *
-     * @param int $tid    群id
+     * @param int $tid 群id
      * @param int $userId 用户id
-     * @param int $itmId  IM群成员ID
-     *
+     * @param int $itmId IM群成员ID
+     * 
      * @returnExample({"result":1})
-     *
+     * 
      * 字段|类型|说明
      * --------|-------|--------------
      * result | int | 结果: 1.成功 0.失败
      *
      * @author wechan
-     *
      * @since 2019年05月06日
      */
     public function afterBuyerJoin(int $tid, int $userId, int $itmId): array
@@ -187,20 +176,19 @@ class Team implements TeamInterface
     }
 
     /**
-     * 买家进群事件.
+     * 买家进群事件
      *
-     * @param int $tid    群id
+     * @param int $tid 群id
      * @param int $userId 用户id
-     * @param int $itmId  IM群成员ID
-     *
+     * @param int $itmId IM群成员ID
+     * 
      * @returnExample({"result":1})
-     *
+     * 
      * 字段|类型|说明
      * --------|-------|--------------
      * result | int | 结果: 1.成功 0.失败
      *
      * @author wechan
-     *
      * @since 2019年05月06日
      */
     public function afterBuyerJoinAsync(int $tid, int $userId, int $itmId)
@@ -209,71 +197,33 @@ class Team implements TeamInterface
     }
 
     /**
-     * 分页取店主群.
+     * 获取店铺激活群数量
      *
-     * @param array $condition
-     * @param int   $page
-     * @param int   $limit
-     *
-     * @return array
-     *
-     * @author zhangyangxun
-     *
-     * @since 2019/5/23
+     * @param integer $storeId 店铺id
+     * @return integer
+     * @internal
+     * 
+     * @author sunanzhi <sunanzhi@hotmail.com>
+     * @since 2019.6.17
      */
-    public function getBuyerTeams(array $condition = [], int $page = 1, int $limit = 20): array
+    public function getStoreActivationTeam(int $storeId): int
     {
-        return EellyClient::request('im/team', 'getBuyerTeams', true, $condition, $page, $limit);
+        return EellyClient::request('im/team', 'getStoreActivationTeam', true, $storeId);
     }
 
     /**
-     * 分页取店主群.
+     * 获取店铺激活群数量
      *
-     * @param array $condition
-     * @param int   $page
-     * @param int   $limit
-     *
-     * @return array
-     *
-     * @author zhangyangxun
-     *
-     * @since 2019/5/23
+     * @param integer $storeId 店铺id
+     * @return integer
+     * @internal
+     * 
+     * @author sunanzhi <sunanzhi@hotmail.com>
+     * @since 2019.6.17
      */
-    public function getBuyerTeamsAsync(array $condition = [], int $page = 1, int $limit = 20)
+    public function getStoreActivationTeamAsync(int $storeId)
     {
-        return EellyClient::request('im/team', 'getBuyerTeams', false, $condition, $page, $limit);
-    }
-
-    /**
-     * 统计店主群.
-     *
-     * @param array $condition
-     *
-     * @return array
-     *
-     * @author zhangyangxun
-     *
-     * @since 2019/5/23
-     */
-    public function getBuyerTeamStat(array $condition = []): array
-    {
-        return EellyClient::request('im/team', 'getBuyerTeamStat', true, $condition);
-    }
-
-    /**
-     * 统计店主群.
-     *
-     * @param array $condition
-     *
-     * @return array
-     *
-     * @author zhangyangxun
-     *
-     * @since 2019/5/23
-     */
-    public function getBuyerTeamStatAsync(array $condition = [])
-    {
-        return EellyClient::request('im/team', 'getBuyerTeamStat', false, $condition);
+        return EellyClient::request('im/team', 'getStoreActivationTeam', false, $storeId);
     }
 
     /**
