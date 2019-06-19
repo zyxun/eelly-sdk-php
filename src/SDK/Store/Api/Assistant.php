ebc225b79f910ef6c0e824f39ff72af0153492e9
sunanzhi <sunanzhi@hotmail.com>
2019-06-19 15:19:54 +0800

    不用继承

--- src/SDK/Store/Api/Assistant.php
+++ src/SDK/Store/Api/Assistant.php
@@ -20,7 +20,7 @@ use Eelly\SDK\Store\DTO\AssistantDTO;
  *
  * @author shadonTools<localhost.shell@gmail.com>
  */
-class Assistant implements AssistantInterface
+class Assistant
 {
     /**
      *
